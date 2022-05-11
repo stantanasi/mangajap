58f7177bc3008a5df414e871e9f26587122fdc96
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-11 16:53:30 +0200

    feat(search): show keyboard on fragment started

--- app/src/main/java/com/tanasi/mangajap/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/tanasi/mangajap/fragments/search/SearchFragment.kt
@@ -1,11 +1,13 @@
 package com.tanasi.mangajap.fragments.search
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
@@ -85,6 +87,11 @@ class SearchFragment : Fragment() {
         binding.toolbar.also {
             (requireActivity() as MainActivity).setSupportActionBar(it)
             it.setNavigationOnClickListener { findNavController().navigateUp() }
+        }
+        binding.etSearch.also {
+            it.requestFocus()
+            val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(it, InputMethodManager.SHOW_IMPLICIT)
         }
 
         viewModel.state.observe(viewLifecycleOwner) { state ->
