8113e13d7eb45c3901daa88af58fd12f3cbe49b3
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-12 14:21:14 +0200

    feat: create SpacingItemDecoration

---
+++ app/src/main/java/com/tanasi/mangajap/ui/SpacingItemDecoration.kt
@@ -0,0 +1,9 @@
+package com.tanasi.mangajap.ui
+
+import androidx.recyclerview.widget.RecyclerView
+
+class SpacingItemDecoration(
+    private val spacing: Int
+) : RecyclerView.ItemDecoration() {
+
+}
