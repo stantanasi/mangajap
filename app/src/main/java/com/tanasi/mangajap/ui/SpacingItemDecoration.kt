2c5eef2613a604f378ce5e1dfadfa6934c95858f
Lory-Stan TANASI <stantanasi@gmail.com>
2022-05-12 14:24:59 +0200

    feat(SpacingItemDecoration): implement spacing for linear RecyclerView

--- app/src/main/java/com/tanasi/mangajap/ui/SpacingItemDecoration.kt
+++ app/src/main/java/com/tanasi/mangajap/ui/SpacingItemDecoration.kt
@@ -1,9 +1,36 @@
 package com.tanasi.mangajap.ui
 
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class SpacingItemDecoration(
     private val spacing: Int
 ) : RecyclerView.ItemDecoration() {
 
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val position = parent.getChildAdapterPosition(view)
+        val count = parent.adapter?.itemCount ?: 0
+
+        when (val layoutManager = parent.layoutManager) {
+            is LinearLayoutManager -> {
+                if (position != count - 1) {
+                    when (layoutManager.orientation) {
+                        RecyclerView.VERTICAL -> outRect.bottom = spacing
+                        RecyclerView.HORIZONTAL -> outRect.right = spacing
+                    }
+                }
+            }
+
+            is GridLayoutManager -> {
+            }
+        }
+    }
 }
