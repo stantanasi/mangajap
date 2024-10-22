f7011061516b354f40d0a4a1fd0e974791408743
Lory-Stan TANASI <stantanasi@gmail.com>
2024-10-22 12:54:54 +0200

    feat(search): add search input

--- app/src/main/java/com/tanasi/mangajap/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/tanasi/mangajap/fragments/search/SearchFragment.kt
@@ -1,9 +1,12 @@
 package com.tanasi.mangajap.fragments.search
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.tanasi.mangajap.databinding.FragmentSearchBinding
@@ -14,19 +17,46 @@ class SearchFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel by viewModels<SearchViewModel>()
-    
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initializeSearch()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+    private fun initializeSearch() {
+        binding.etSearch.apply {
+            setOnEditorActionListener { _, actionId, _ ->
+                when (actionId) {
+                    EditorInfo.IME_ACTION_SEARCH -> {
+                        requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE)
+                            .let { it as? InputMethodManager }
+                            ?.hideSoftInputFromWindow(this.windowToken, 0)
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+        }
+
+        binding.btnSearchClear.setOnClickListener {
+            binding.etSearch.setText("")
+        }
     }
 }
