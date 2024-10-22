28127bd87ae0b1ccb33285439210a89f4064b278
Lory-Stan TANASI <stantanasi@gmail.com>
2024-10-22 18:54:02 +0200

    feat(manga): display manga chapters

--- app/src/main/java/com/tanasi/mangajap/fragments/manga/MangaChaptersFragment.kt
+++ app/src/main/java/com/tanasi/mangajap/fragments/manga/MangaChaptersFragment.kt
@@ -6,7 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import com.tanasi.mangajap.adapters.AppAdapter
 import com.tanasi.mangajap.databinding.FragmentMangaChaptersBinding
+import com.tanasi.mangajap.models.Manga
+import kotlinx.coroutines.launch
 
 class MangaChaptersFragment : Fragment() {
 
@@ -16,6 +22,8 @@ class MangaChaptersFragment : Fragment() {
     private val viewModel by viewModels<MangaViewModel>(
         ownerProducer = { requireParentFragment() }
     )
+
+    private val appAdapter = AppAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,10 +36,37 @@ class MangaChaptersFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initializeMangaChapters()
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.state.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED).collect { state ->
+                when (state) {
+                    is MangaViewModel.State.SuccessLoading -> {
+                        displayMangaChapters(state.manga)
+                    }
+
+                    else -> {}
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+    private fun initializeMangaChapters() {
+        binding.rvMangaChapters.apply {
+            adapter = appAdapter
+        }
+    }
+
+    private fun displayMangaChapters(manga: Manga) {
+        appAdapter.submitList(manga.chapters.onEach {
+            it.itemType = AppAdapter.Type.CHAPTER_ITEM
+        })
     }
 }
