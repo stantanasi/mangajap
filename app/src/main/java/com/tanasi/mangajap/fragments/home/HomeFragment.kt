60bd21bf7c2fc8fa4556653b0adbb63cf89ad1d0
Lory-Stan TANASI <stantanasi@gmail.com>
2024-10-22 11:01:48 +0200

    feat: create home fragment

---
+++ app/src/main/java/com/tanasi/mangajap/fragments/home/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.tanasi.mangajap.fragments.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.tanasi.mangajap.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel by viewModels<HomeViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/tanasi/mangajap/fragments/home/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package com.tanasi.mangajap.fragments.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+}
