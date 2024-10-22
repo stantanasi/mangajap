4857c4ac5e9be2499dce14d20ddaf3550c2ba377
Lory-Stan TANASI <stantanasi@gmail.com>
2024-10-22 16:31:55 +0200

    feat(manga): remove UI from manga

--- app/src/main/java/com/tanasi/mangajap/fragments/manga/MangaViewModel.kt
+++ app/src/main/java/com/tanasi/mangajap/fragments/manga/MangaViewModel.kt
@@ -3,110 +3,38 @@ package com.tanasi.mangajap.fragments.manga
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.tanasi.jsonapi.JsonApiResponse
 import com.tanasi.mangajap.models.Manga
-import com.tanasi.mangajap.models.MangaEntry
-import com.tanasi.mangajap.utils.MangaJapApi
+import com.tanasi.mangajap.utils.MangaReader
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 
 class MangaViewModel(id: String) : ViewModel() {
 
     private val _state: MutableStateFlow<State> = MutableStateFlow(State.Loading)
-    private val _updating: MutableStateFlow<SavingState?> = MutableStateFlow(null)
-    val state: Flow<State> = combine(
-        _state,
-        _updating,
-    ) { state, updating ->
-        when (state) {
-            is State.SuccessLoading -> {
-                when (updating) {
-                    is SavingState.SuccessSaving -> {
-                        State.SuccessLoading(
-                            manga = state.manga.copy(
-                                mangaEntry = updating.mangaEntry,
-                            )
-                        )
-                    }
-                    else -> state
-                }
-            }
-            else -> state
-        }
-    }
+    val state: Flow<State> = _state
 
     sealed class State {
         data object Loading : State()
         data class SuccessLoading(val manga: Manga) : State()
-        data class FailedLoading(val error: JsonApiResponse.Error) : State()
-    }
-
-    sealed class SavingState {
-        data object Saving : SavingState()
-        data class SuccessSaving(val mangaEntry: MangaEntry) : SavingState()
-        data class FailedSaving(val error: JsonApiResponse.Error) : SavingState()
+        data class FailedLoading(val error: Exception) : State()
     }
 
     init {
         getManga(id)
     }
 
-    private fun getManga(id: String) = viewModelScope.launch(Dispatchers.IO) {
+    fun getManga(id: String) = viewModelScope.launch(Dispatchers.IO) {
         _state.emit(State.Loading)
 
         try {
-            val response = MangaJapApi.Manga.details(
-                id,
-                include = listOf(
-                    "manga-entry",
-                    "genres",
-                    "themes",
-                    "volumes",
-                    "staff.people",
-                    "franchises.destination"
-                )
-            )
+            val manga = MangaReader.getManga(id)
 
-            when (response) {
-                is JsonApiResponse.Success -> {
-                    _state.emit(State.SuccessLoading(response.body.data!!))
-                }
-
-                is JsonApiResponse.Error -> {
-                    _state.emit(State.FailedLoading(response))
-                }
-            }
+            _state.emit(State.SuccessLoading(manga))
         } catch (e: Exception) {
             Log.e("MangaViewModel", "getManga: ", e)
-            _state.emit(State.FailedLoading(JsonApiResponse.Error.UnknownError(e)))
-        }
-    }
-
-    fun saveMangaEntry(mangaEntry: MangaEntry) = viewModelScope.launch(Dispatchers.IO) {
-        _updating.emit(SavingState.Saving)
-
-        try {
-            val id = mangaEntry.id
-            val response = if (id == null) {
-                MangaJapApi.MangaEntries.create(mangaEntry)
-            } else {
-                MangaJapApi.MangaEntries.update(id, mangaEntry)
-            }
-
-            when (response) {
-                is JsonApiResponse.Success -> {
-                    _updating.emit(SavingState.SuccessSaving(response.body.data!!))
-                }
-                is JsonApiResponse.Error -> {
-                    _updating.emit(SavingState.FailedSaving(response))
-                }
-            }
-        } catch (e: Exception) {
-            Log.e("MangaViewModel", "updateMangaEntry: ", e)
-            _updating.emit(SavingState.FailedSaving(JsonApiResponse.Error.UnknownError(e)))
+            _state.emit(State.FailedLoading(e))
         }
     }
 }
