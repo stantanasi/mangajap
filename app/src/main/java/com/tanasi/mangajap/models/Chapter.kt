19d0ad46c6ddecd826625b9934a771804e79283b
Lory-Stan TANASI <stantanasi@gmail.com>
2024-10-24 19:05:15 +0200

    feat(mangareader): retrieve chapters languages

--- app/src/main/java/com/tanasi/mangajap/models/Chapter.kt
+++ app/src/main/java/com/tanasi/mangajap/models/Chapter.kt
@@ -5,7 +5,8 @@ import com.tanasi.mangajap.adapters.AppAdapter
 data class Chapter(
     val id: String,
     val number: Double,
-    val title: String = "",
+    val title: String? = null,
+    val language: String? = null,
 ) : AppAdapter.Item {
 
     override lateinit var itemType: AppAdapter.Type
