b6d424e4dddeaf7408e03188cdb1a025cd70db58
Lory-Stan TANASI <stantanasi@gmail.com>
2024-10-24 13:56:54 +0200

    perf: delete undisplayed dialogs

--- app/src/main/java/com/tanasi/mangajap/utils/extensions/Bitmap.kt
+++ app/src/main/java/com/tanasi/mangajap/utils/extensions/Bitmap.kt
@@ -7,7 +7,6 @@ import android.os.Environment
 import android.util.Base64
 import android.util.Log
 import androidx.palette.graphics.Palette
-import com.tanasi.mangajap.ui.SingleMediaScanner
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.ByteArrayOutputStream
@@ -37,27 +36,6 @@ suspend fun Bitmap.toCache(file: File): Boolean = withContext(Dispatchers.IO) {
         true
     } catch (e: IOException) {
         Log.e("Function", "bitmapToCache: ", e)
-        false
-    }
-}
-
-fun Bitmap.save(context: Context, name: String, extension: String): Boolean {
-    return try {
-        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath
-        var file = File(path, "$name.$extension")
-        var i = 1
-        while (file.exists()) {
-            file = File(path, "$name (${i++}).$extension")
-        }
-
-        val out = FileOutputStream(file)
-        this.compress(Bitmap.CompressFormat.JPEG, 90, out)
-        out.flush()
-        out.close()
-        SingleMediaScanner(context, file)
-        true
-    } catch (e: Exception) {
-        Log.e("ImageActivity", "saveImage: ", e)
         false
     }
 }
