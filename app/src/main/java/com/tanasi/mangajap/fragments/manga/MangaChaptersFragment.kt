c32e7aa6d28091faeab4b949135da6a9f561e3df
Lory-Stan TANASI <stantanasi@gmail.com>
2024-10-24 19:05:34 +0200

    feat(manga-chapters): filter chapters by language

--- app/src/main/java/com/tanasi/mangajap/fragments/manga/MangaChaptersFragment.kt
+++ app/src/main/java/com/tanasi/mangajap/fragments/manga/MangaChaptersFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -14,6 +16,7 @@ import com.tanasi.mangajap.adapters.AppAdapter
 import com.tanasi.mangajap.databinding.FragmentMangaChaptersBinding
 import com.tanasi.mangajap.models.Chapter
 import kotlinx.coroutines.launch
+import java.util.Locale
 
 class MangaChaptersFragment : Fragment() {
 
@@ -41,35 +44,36 @@ class MangaChaptersFragment : Fragment() {
         initializeMangaChapters()
 
         viewLifecycleOwner.lifecycleScope.launch {
-            viewModel.chapters.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED).collect { state ->
-                when (state) {
-                    MangaViewModel.ChaptersState.Loading -> binding.isLoading.apply {
-                        root.visibility = View.VISIBLE
-                        pbIsLoading.visibility = View.VISIBLE
-                        gIsLoadingRetry.visibility = View.GONE
-                    }
+            viewModel.chapters.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+                .collect { state ->
+                    when (state) {
+                        MangaViewModel.ChaptersState.Loading -> binding.isLoading.apply {
+                            root.visibility = View.VISIBLE
+                            pbIsLoading.visibility = View.VISIBLE
+                            gIsLoadingRetry.visibility = View.GONE
+                        }
 
-                    is MangaViewModel.ChaptersState.SuccessLoading -> {
-                        displayMangaChapters(state.chapters)
-                        binding.isLoading.root.visibility = View.GONE
-                    }
+                        is MangaViewModel.ChaptersState.SuccessLoading -> {
+                            displayMangaChapters(state.chapters)
+                            binding.isLoading.root.visibility = View.GONE
+                        }
 
-                    is MangaViewModel.ChaptersState.FailedLoading -> {
-                        Toast.makeText(
-                            requireContext(),
-                            state.error.message ?: "",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        binding.isLoading.apply {
-                            pbIsLoading.visibility = View.GONE
-                            gIsLoadingRetry.visibility = View.VISIBLE
-                            btnIsLoadingRetry.setOnClickListener {
-                                viewModel.getMangaChapters(viewModel.id)
+                        is MangaViewModel.ChaptersState.FailedLoading -> {
+                            Toast.makeText(
+                                requireContext(),
+                                state.error.message ?: "",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            binding.isLoading.apply {
+                                pbIsLoading.visibility = View.GONE
+                                gIsLoadingRetry.visibility = View.VISIBLE
+                                btnIsLoadingRetry.setOnClickListener {
+                                    viewModel.getMangaChapters(viewModel.id)
+                                }
                             }
                         }
                     }
                 }
-            }
         }
     }
 
@@ -86,8 +90,57 @@ class MangaChaptersFragment : Fragment() {
     }
 
     private fun displayMangaChapters(chapters: List<Chapter>) {
-        appAdapter.submitList(chapters.onEach {
-            it.itemType = AppAdapter.Type.CHAPTER_ITEM
-        })
+        class Language(
+            val code: String,
+            val name: String,
+        )
+
+        val languages = chapters
+            .distinctBy { it.language }
+            .mapNotNull { it.language }
+            .map {
+                val locale = Locale(it)
+
+                Language(
+                    code = it,
+                    name = locale.getDisplayLanguage(locale),
+                )
+            }
+
+        binding.sMangaChaptersLanguage.apply {
+            adapter = ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                languages.map { it.name }.toTypedArray(),
+            ).also {
+                it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            }
+
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    appAdapter.submitList(
+                        chapters
+                            .filter { it.language == languages[position].code }
+                            .onEach { it.itemType = AppAdapter.Type.CHAPTER_ITEM }
+                    )
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+            }
+
+            setSelection(0)
+        }
+
+        appAdapter.submitList(
+            chapters
+                .filter { it.language == languages.firstOrNull()?.code }
+                .onEach { it.itemType = AppAdapter.Type.CHAPTER_ITEM }
+        )
     }
 }
