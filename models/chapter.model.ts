3f5d426d7eea0285e80c43f5d615aec763abbed9
Lory-Stan TANASI <stantanasi@gmail.com>
2025-03-11 16:03:29 +0100

    feat: create chapter model

---
+++ models/chapter.model.ts
@@ -0,0 +1,58 @@
+import { model, Schema } from "@stantanasi/jsonapi-client";
+
+export interface IChapter {
+  id: string;
+
+  number: number;
+  title: string;
+  overview: string;
+  publishedDate: Date;
+  cover: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const ChapterSchema = new Schema<IChapter>({
+  attributes: {
+    number: {},
+
+    title: {},
+
+    overview: {},
+
+    publishedDate: {
+      get: function (value: string) {
+        return new Date(value);
+      },
+      transform: function (val) {
+        return val.toISOString().slice(0, 10);
+      },
+    },
+
+    cover: {},
+
+    createdAt: {
+      get: function (value: string) {
+        return new Date(value);
+      },
+      transform: function (val) {
+        return val.toISOString();
+      },
+    },
+
+    updatedAt: {
+      get: function (value: string) {
+        return new Date(value);
+      },
+      transform: function (val) {
+        return val.toISOString();
+      },
+    },
+  },
+
+  relationships: {
+  },
+});
+
+
+export default class Chapter extends model<IChapter>("chapters", ChapterSchema) { }
