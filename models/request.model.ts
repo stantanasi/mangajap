17c2f425292cacd37ba18c2fea3e887f3f99e874
Lory-Stan TANASI <stantanasi@gmail.com>
2025-03-11 16:10:45 +0100

    feat: create request model

---
+++ models/request.model.ts
@@ -0,0 +1,48 @@
+import { model, Schema } from "@stantanasi/jsonapi-client";
+
+export interface IRequest {
+  id: string;
+
+  requestType: string;
+  data: string;
+  isDone: boolean;
+  userHasRead: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const RequestSchema = new Schema<IRequest>({
+  attributes: {
+    requestType: {},
+
+    data: {},
+
+    isDone: {},
+
+    userHasRead: {},
+
+    createdAt: {
+      get: function (value: string) {
+        return new Date(value);
+      },
+      transform: function (val) {
+        return val.toISOString();
+      },
+    },
+
+    updatedAt: {
+      get: function (value: string) {
+        return new Date(value);
+      },
+      transform: function (val) {
+        return val.toISOString();
+      },
+    },
+  },
+
+  relationships: {
+  },
+});
+
+
+export default class Request extends model<IRequest>("requests", RequestSchema) { }
