16f3b3db51d05e5fd13df2a856380c7e753ad850
Lory-Stan TANASI <stantanasi@gmail.com>
2025-03-12 11:52:20 +0100

    feat(search): implement searching

--- screens/search/SearchScreen.tsx
+++ screens/search/SearchScreen.tsx
@@ -1,16 +1,67 @@
-import { StaticScreenProps } from '@react-navigation/native';
-import { StyleSheet } from 'react-native';
+import { StaticScreenProps, useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import AnimeCard from '../../components/molecules/AnimeCard';
+import MangaCard from '../../components/molecules/MangaCard';
+import { Anime, Manga } from '../../models';
 
 type Props = StaticScreenProps<{}>;
 
 export default function SearchScreen({ route }: Props) {
+  const navigation = useNavigation();
+  const [query, setQuery] = useState('');
+  const [data, setData] = useState<(Anime | Manga)[]>([]);
+
   return (
     <SafeAreaView style={styles.container}>
+      <TextInput
+        value={query}
+        onChangeText={(text) => setQuery(text)}
+        onSubmitEditing={async () => {
+          const animes = await Anime.find({
+            query: query,
+          });
+          const mangas = await Manga.find({
+            query: query,
+          });
+
+          setData([...animes, ...mangas]);
+        }}
+        placeholder="Rechercher"
+        placeholderTextColor="#a1a1a1"
+        style={styles.search}
+      />
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          item.type == Anime.type ? (
+            <AnimeCard
+              anime={item as Anime}
+              onPress={() => navigation.navigate('Anime', { id: item.id })}
+            />
+          ) : (
+            <MangaCard
+              manga={item as Manga}
+              onPress={() => navigation.navigate('Manga', { id: item.id })}
+            />
+          )
+        )}
+      />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {},
+  search: {
+    alignItems: 'center',
+    borderColor: '#d1d5db',
+    borderWidth: 1,
+    borderRadius: 8,
+    margin: 16,
+    padding: 8,
+  },
 });
