7843d26af5a263cda419619fbf2d9b2080e2d7da
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-08 17:58:59 +0200

    feat(profile): update design
    Display anime/manga libraries
    Display anime/manga favorites

--- screens/profile/ProfileScreen.tsx
+++ screens/profile/ProfileScreen.tsx
@@ -1,7 +1,9 @@
-import { StaticScreenProps } from '@react-navigation/native';
+import { StaticScreenProps, useNavigation } from '@react-navigation/native';
 import { useContext, useEffect, useState } from 'react';
-import { ActivityIndicator, Modal, Pressable, StyleSheet, Text, TextInput } from 'react-native';
+import { ActivityIndicator, FlatList, Image, Modal, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import AnimeCard from '../../components/molecules/AnimeCard';
+import MangaCard from '../../components/molecules/MangaCard';
 import { AuthContext } from '../../contexts/AuthContext';
 import { User } from '../../models';
 
@@ -129,6 +131,7 @@ type Props = StaticScreenProps<{
 }>;
 
 export default function ProfileScreen({ route }: Props) {
+  const navigation = useNavigation();
   const { user: authenticatedUser, logout } = useContext(AuthContext);
   const [user, setUser] = useState<User>();
   const [isLoginModalVisible, setLoginModalVisible] = useState(false);
@@ -209,37 +212,277 @@ export default function ProfileScreen({ route }: Props) {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text
-        style={{
-          fontSize: 24,
-          marginTop: 24,
-          textAlign: 'center',
+      <ScrollView
+        contentContainerStyle={{
+          paddingVertical: 16,
         }}
       >
-        {user.pseudo}
-      </Text>
+        <View
+          style={{
+            alignItems: 'center',
+            flexDirection: 'row',
+            marginHorizontal: 16,
+          }}
+        >
+          <Image
+            source={{ uri: user.avatar ?? undefined }}
+            style={styles.avatar}
+          />
 
-      <Text
-        onPress={() => logout()}
-        style={{
-          alignSelf: 'center',
-          backgroundColor: '#000',
-          borderColor: '#000',
-          borderRadius: 360,
-          borderWidth: 1,
-          color: '#fff',
-          marginTop: 20,
-          paddingHorizontal: 12,
-          paddingVertical: 4,
-          textAlign: 'center',
-        }}
-      >
-        Se déconnecter
-      </Text>
-    </SafeAreaView>
+          <View
+            style={{
+              flex: 1,
+              marginLeft: 16,
+            }}
+          >
+            <Text style={styles.pseudo}>
+              {user.pseudo}
+            </Text>
+
+            <Text style={styles.bio}>
+              {user.about}
+            </Text>
+
+            <Text
+              style={{
+                color: '#888',
+                fontSize: 13,
+                marginTop: 6,
+              }}
+            >
+              <Text>
+                <Text style={{ color: '#000', fontWeight: 'bold' }}>{user.followersCount}</Text>
+                <Text> abonnées</Text>
+              </Text>
+              <Text style={{ color: '#000', fontWeight: 'bold' }}> • </Text>
+              <Text>
+                <Text style={{ color: '#000', fontWeight: 'bold' }}>{user.followingCount}</Text>
+                <Text> abonnements</Text>
+              </Text>
+            </Text>
+          </View>
+        </View>
+
+        <Text
+          onPress={() => logout()}
+          style={{
+            alignSelf: 'flex-start',
+            borderColor: '#000',
+            borderRadius: 360,
+            borderWidth: 1,
+            color: '#000',
+            fontWeight: 'bold',
+            marginBottom: 10,
+            marginHorizontal: 16,
+            marginTop: 20,
+            paddingHorizontal: 12,
+            paddingVertical: 4,
+          }}
+        >
+          Déconnexion
+        </Text>
+
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: 'bold',
+            marginBottom: 6,
+            marginHorizontal: 16,
+            marginTop: 20,
+          }}
+        >
+          Statistiques
+        </Text>
+
+        <FlatList
+          horizontal
+          data={[
+            { label: 'Épisodes vus', value: user.episodesWatch },
+            { label: 'Tomes lus', value: user.volumesRead },
+            { label: 'Chapitres lus', value: user.chaptersRead },
+          ]}
+          keyExtractor={(item) => item.label}
+          renderItem={({ item }) => (
+            <View
+              style={{
+                minWidth: 200,
+                alignItems: 'center',
+                borderColor: '#ccc',
+                borderRadius: 4,
+                borderWidth: 1,
+                gap: 6,
+                paddingHorizontal: 16,
+                paddingVertical: 6,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 13,
+                  fontWeight: 'bold',
+                  marginHorizontal: 10,
+                }}
+              >
+                {item.label}
+              </Text>
+
+              <View style={{ width: '100%', height: 1, backgroundColor: '#ccc' }} />
+
+              <Text
+                style={{
+                  fontSize: 24,
+                  fontWeight: 'bold',
+                  marginHorizontal: 10,
+                }}
+              >
+                {item.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
+              </Text>
+            </View>
+          )}
+          ItemSeparatorComponent={() => <View style={{ width: 6 }} />}
+          ListHeaderComponent={() => <View style={{ width: 16 }} />}
+          ListFooterComponent={() => <View style={{ width: 16 }} />}
+        />
+
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: 'bold',
+            marginBottom: 6,
+            marginHorizontal: 16,
+            marginTop: 20,
+          }}
+        >
+          Animes
+        </Text>
+
+        <FlatList
+          horizontal
+          data={user['anime-library']!.map((entry) => entry.anime!.copy({
+            'anime-entry': entry,
+          }))}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <AnimeCard
+              anime={item}
+              onPress={() => navigation.navigate('Anime', { id: item.id })}
+            />
+          )}
+          ItemSeparatorComponent={() => <View style={{ width: 6 }} />}
+          ListHeaderComponent={() => <View style={{ width: 16 }} />}
+          ListFooterComponent={() => <View style={{ width: 16 }} />}
+        />
+
+        {user['anime-favorites']!.length > 0 ? (
+          <>
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: 'bold',
+                marginBottom: 6,
+                marginHorizontal: 16,
+                marginTop: 20,
+              }}
+            >
+              Animes préférées
+            </Text>
+
+            <FlatList
+              horizontal
+              data={user['anime-favorites']!.map((entry) => entry.anime!.copy({
+                'anime-entry': entry,
+              }))}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => (
+                <AnimeCard
+                  anime={item}
+                  onPress={() => navigation.navigate('Anime', { id: item.id })}
+                />
+              )}
+              ItemSeparatorComponent={() => <View style={{ width: 6 }} />}
+              ListHeaderComponent={() => <View style={{ width: 16 }} />}
+              ListFooterComponent={() => <View style={{ width: 16 }} />}
+            />
+          </>
+        ) : null}
+
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: 'bold',
+            marginBottom: 6,
+            marginHorizontal: 16,
+            marginTop: 20,
+          }}
+        >
+          Mangas
+        </Text>
+
+        <FlatList
+          horizontal
+          data={user['manga-library']!.map((entry) => entry.manga!.copy({
+            'manga-entry': entry,
+          }))}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <MangaCard
+              manga={item}
+              onPress={() => navigation.navigate('Manga', { id: item.id })}
+            />
+          )}
+          ItemSeparatorComponent={() => <View style={{ width: 6 }} />}
+          ListHeaderComponent={() => <View style={{ width: 16 }} />}
+          ListFooterComponent={() => <View style={{ width: 16 }} />}
+        />
+
+        {user['manga-favorites']!.length > 0 ? (
+          <>
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: 'bold',
+                marginBottom: 6,
+                marginHorizontal: 16,
+                marginTop: 20,
+              }}
+            >
+              Mangas préférées
+            </Text>
+
+            <FlatList
+              horizontal
+              data={user['manga-favorites']!.map((entry) => entry.manga!.copy({
+                'manga-entry': entry,
+              }))}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => (
+                <MangaCard
+                  manga={item}
+                  onPress={() => navigation.navigate('Manga', { id: item.id })}
+                />
+              )}
+              ItemSeparatorComponent={() => <View style={{ width: 6 }} />}
+              ListHeaderComponent={() => <View style={{ width: 16 }} />}
+              ListFooterComponent={() => <View style={{ width: 16 }} />}
+            />
+          </>
+        ) : null}
+      </ScrollView>
+    </SafeAreaView >
   );
 }
 
 const styles = StyleSheet.create({
   container: {},
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 360,
+  },
+  pseudo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  bio: {
+    color: '#444',
+  },
 });
