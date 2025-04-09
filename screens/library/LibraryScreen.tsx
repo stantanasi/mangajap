da74a79b1f111413bb2005936203c26de981052a
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-09 22:47:29 +0200

    feat(library): retrieve libraries

--- screens/library/LibraryScreen.tsx
+++ screens/library/LibraryScreen.tsx
@@ -1,7 +1,8 @@
 import { StaticScreenProps } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { AnimeEntry, MangaEntry, User } from '../../models';
 
 type Props = StaticScreenProps<{
   type: 'anime-library' | 'manga-library' | 'anime-favorites' | 'manga-favorites';
@@ -9,6 +10,47 @@ type Props = StaticScreenProps<{
 }>;
 
 export default function LibraryScreen({ route }: Props) {
+  const [library, setLibrary] = useState<(AnimeEntry | MangaEntry)[]>();
+
+  useEffect(() => {
+    const prepare = async () => {
+      setLibrary(undefined);
+
+      if (route.params.type === 'anime-library') {
+        const animeLibrary = await User.findById(route.params.userId).get('anime-library')
+          .include(['anime'])
+          .sort({ updatedAt: 'desc' })
+          .limit(500);
+
+        setLibrary(animeLibrary);
+      } else if (route.params.type === 'anime-favorites') {
+        const animeFavorites = await User.findById(route.params.userId).get('anime-favorites')
+          .include(['anime'])
+          .sort({ updatedAt: 'desc' })
+          .limit(500);
+
+        setLibrary(animeFavorites);
+      } else if (route.params.type === 'manga-library') {
+        const mangaLibrary = await User.findById(route.params.userId).get('manga-library')
+          .include(['manga'])
+          .sort({ updatedAt: 'desc' })
+          .limit(500);
+
+        setLibrary(mangaLibrary);
+      } else {
+        const mangaFavorites = await User.findById(route.params.userId).get('manga-favorites')
+          .include(['manga'])
+          .sort({ updatedAt: 'desc' })
+          .limit(500);
+
+        setLibrary(mangaFavorites);
+      }
+    };
+
+    prepare()
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
     </SafeAreaView>
