eeb64939d3c218bbbcca67066963666112f95eaa
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-09 14:58:10 +0200

    feat(follows): retrieve user followers or following

--- screens/follows/FollowsScreen.tsx
+++ screens/follows/FollowsScreen.tsx
@@ -1,7 +1,8 @@
 import { StaticScreenProps } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Follow, User } from '../../models';
 
 type Props = StaticScreenProps<{
   type: 'followers' | 'following';
@@ -9,6 +10,29 @@ type Props = StaticScreenProps<{
 }>;
 
 export default function FollowsScreen({ route }: Props) {
+  const [follows, setFollows] = useState<Follow[]>();
+
+  useEffect(() => {
+    const prepare = async () => {
+      const userId = route.params.userId;
+
+      if (route.params.type === 'followers') {
+        const followers = await User.findById(userId).get('followers')
+          .include(['follower']);
+
+        setFollows(followers);
+      } else {
+        const following = await User.findById(userId).get('following')
+          .include(['followed']);
+
+        setFollows(following);
+      }
+    };
+
+    prepare()
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
     </SafeAreaView>
