e177930714914f5dd13caf26545fe6426224d244
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-10 10:04:10 +0200

    feat(people): retrieve people data

--- screens/people/PeopleScreen.tsx
+++ screens/people/PeopleScreen.tsx
@@ -1,13 +1,33 @@
 import { StaticScreenProps } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { People } from '../../models';
 
 type Props = StaticScreenProps<{
   id: string;
 }>;
 
 export default function PeopleScreen({ route }: Props) {
+  const [people, setPeople] = useState<People>();
+
+  useEffect(() => {
+    const prepare = async () => {
+      setPeople(undefined);
+
+      const people = await People.findById(route.params.id)
+        .include([
+          'staff.anime',
+          'staff.manga',
+        ]);
+
+      setPeople(people);
+    };
+
+    prepare()
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
     </SafeAreaView>
