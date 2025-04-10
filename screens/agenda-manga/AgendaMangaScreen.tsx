6305ea77725e693721d6c079945917067f8d878a
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-10 14:25:48 +0200

    feat(agenda-manga): retrieve agenda manga data

--- screens/agenda-manga/AgendaMangaScreen.tsx
+++ screens/agenda-manga/AgendaMangaScreen.tsx
@@ -1,11 +1,43 @@
 import { StaticScreenProps } from '@react-navigation/native';
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { AuthContext } from '../../contexts/AuthContext';
+import { Manga, User } from '../../models';
 
 type Props = StaticScreenProps<{}>;
 
 export default function AgendaMangaScreen({ }: Props) {
+  const { user } = useContext(AuthContext);
+  const [mangas, setMangas] = useState<Manga[]>();
+
+  useEffect(() => {
+    const prepare = async () => {
+      setMangas(undefined);
+
+      if (user) {
+        const mangaLibrary = await User.findById(user.id).get('manga-library')
+          .include(['manga'])
+          .sort({ updatedAt: 'desc' })
+          .limit(500);
+
+        const mangas = mangaLibrary
+          .filter((entry) => {
+            const progress = (entry.chaptersRead / entry.manga!.chapterCount) * 100;
+            return progress < 100;
+          })
+          .map((entry) => entry.manga!.copy({
+            'manga-entry': entry,
+          }));
+
+        setMangas(mangas);
+      }
+    };
+
+    prepare()
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
     </SafeAreaView>
