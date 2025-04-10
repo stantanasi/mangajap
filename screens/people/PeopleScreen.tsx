67939641d5f98ba141bbf6e9a011a7f4317fba67
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-10 10:11:43 +0200

    feat(people): display people

--- screens/people/PeopleScreen.tsx
+++ screens/people/PeopleScreen.tsx
@@ -1,14 +1,33 @@
-import { StaticScreenProps } from '@react-navigation/native';
+import { StaticScreenProps, useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { ActivityIndicator, FlatList, Image, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { People } from '../../models';
+import AnimeSearchCard from '../../components/molecules/AnimeSearchCard';
+import MangaSearchCard from '../../components/molecules/MangaSearchCard';
+import { Anime, People } from '../../models';
+
+const Header = ({ people }: { people: People }) => {
+  return (
+    <View style={styles.header}>
+      <Image
+        source={{ uri: people.portrait ?? undefined }}
+        style={styles.image}
+      />
+
+      <Text style={styles.name}>
+        {people.name}
+      </Text>
+    </View>
+  );
+};
+
 
 type Props = StaticScreenProps<{
   id: string;
 }>;
 
 export default function PeopleScreen({ route }: Props) {
+  const navigation = useNavigation();
   const [people, setPeople] = useState<People>();
 
   useEffect(() => {
@@ -28,12 +47,75 @@ export default function PeopleScreen({ route }: Props) {
       .catch((err) => console.error(err));
   }, []);
 
+  if (!people) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ActivityIndicator
+          animating
+          color="#000"
+          size="large"
+        />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      <FlatList
+        data={people.staff!.map((staff) => {
+          if (staff.anime) {
+            return staff.anime;
+          } else if (staff.manga) {
+            return staff.manga;
+          }
+          return null;
+        }).filter((media) => !!media)}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          item instanceof Anime ? (
+            <AnimeSearchCard
+              anime={item}
+              onPress={() => navigation.navigate('Anime', { id: item.id })}
+              style={{
+                marginHorizontal: 10,
+              }}
+            />
+          ) : (
+            <MangaSearchCard
+              manga={item}
+              onPress={() => navigation.navigate('Manga', { id: item.id })}
+              style={{
+                marginHorizontal: 10,
+              }}
+            />
+          )
+        )}
+        ListHeaderComponent={Header({
+          people: people,
+        })}
+        ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+      />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {},
+  header: {
+    margin: 16,
+  },
+  image: {
+    width: 180,
+    alignSelf: 'center',
+    aspectRatio: 1 / 1,
+    backgroundColor: '#ccc',
+    borderRadius: 360,
+  },
+  name: {
+    color: '#000',
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginTop: 16,
+    textAlign: 'center',
+  },
 });
