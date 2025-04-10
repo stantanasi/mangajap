1f0a758921b5c81964a210a17f93310520111370
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-10 15:01:50 +0200

    feat(agenda-manga): prevent displaying data if not logged in

--- screens/agenda-manga/AgendaMangaScreen.tsx
+++ screens/agenda-manga/AgendaMangaScreen.tsx
@@ -1,10 +1,10 @@
 import { StaticScreenProps, useNavigation } from '@react-navigation/native';
 import React, { useContext, useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, StyleSheet, View } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import {AuthContext} from '../../contexts/AuthContext';
-import { Manga, User } from '../../models';
 import MangaAgendaCard from '../../components/molecules/MangaAgendaCard';
+import { AuthContext } from '../../contexts/AuthContext';
+import { Manga, User } from '../../models';
 
 type Props = StaticScreenProps<{}>;
 
@@ -38,7 +38,43 @@ export default function AgendaMangaScreen({ }: Props) {
 
     prepare()
       .catch((err) => console.error(err));
-  }, []);
+  }, [user]);
+
+  if (!user) {
+    return (
+      <SafeAreaView
+        style={[styles.container, { alignItems: 'center', justifyContent: 'center' }]}
+      >
+        <Text
+          style={{
+            fontSize: 26,
+            fontWeight: 'bold',
+            marginBottom: 40,
+            marginHorizontal: 16,
+          }}
+        >
+          Vous n'êtes pas connectés
+        </Text>
+
+        <Text
+          onPress={() => navigation.navigate('Profile', {})}
+          style={{
+            borderColor: '#000',
+            borderRadius: 360,
+            borderWidth: 2,
+            color: '#000',
+            fontSize: 16,
+            fontWeight: 'bold',
+            marginHorizontal: 16,
+            paddingHorizontal: 25,
+            paddingVertical: 15,
+          }}
+        >
+          Se connecter
+        </Text>
+      </SafeAreaView>
+    );
+  }
 
   if (!mangas) {
     return (
@@ -75,5 +111,7 @@ export default function AgendaMangaScreen({ }: Props) {
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+  },
 });
