c132ec531d108fece508b66c7d9469db11cbd2b0
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-10 14:21:41 +0200

    feat(agenda-anime): display agenda anime

--- screens/agenda-anime/AgendaAnimeScreen.tsx
+++ screens/agenda-anime/AgendaAnimeScreen.tsx
@@ -1,13 +1,15 @@
-import { StaticScreenProps } from '@react-navigation/native';
+import { StaticScreenProps, useNavigation } from '@react-navigation/native';
 import React, { useContext, useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import AnimeAgendaCard from '../../components/molecules/AnimeAgendaCard';
 import { AuthContext } from '../../contexts/AuthContext';
 import { Anime, User } from '../../models';
 
 type Props = StaticScreenProps<{}>;
 
 export default function AgendaAnimeScreen({ }: Props) {
+  const navigation = useNavigation();
   const { user } = useContext(AuthContext);
   const [animes, setAnimes] = useState<Anime[]>();
 
@@ -38,8 +40,36 @@ export default function AgendaAnimeScreen({ }: Props) {
       .catch((err) => console.error(err));
   }, []);
 
+  if (!animes) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ActivityIndicator
+          animating
+          color="#000"
+          size="large"
+        />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      <FlatList
+        data={animes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <AnimeAgendaCard
+            anime={item}
+            onPress={() => navigation.navigate('Anime', { id: item.id })}
+            style={{
+              marginHorizontal: 16,
+            }}
+          />
+        )}
+        ListHeaderComponent={() => <View style={{ height: 16 }} />}
+        ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+        ListFooterComponent={() => <View style={{ height: 16 }} />}
+      />
     </SafeAreaView>
   );
 }
