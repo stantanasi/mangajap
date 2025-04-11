2b444312da3203bf7170599abc77753b7e53daa6
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-11 19:09:18 +0200

    feat(profile-edit): display profile edit

--- screens/profile-edit/ProfileEditScreen.tsx
+++ screens/profile-edit/ProfileEditScreen.tsx
@@ -1,6 +1,7 @@
-import { StaticScreenProps } from '@react-navigation/native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { StaticScreenProps, useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { ActivityIndicator, Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { User } from '../../models';
 import { IUser } from '../../models/user.model';
@@ -10,6 +11,7 @@ type Props = StaticScreenProps<{
 }>;
 
 export default function ProfileEditScreen({ route }: Props) {
+  const navigation = useNavigation();
   const [user, setUser] = useState<User>();
   const [form, setForm] = useState<IUser>(undefined as any);
 
@@ -25,12 +27,117 @@ export default function ProfileEditScreen({ route }: Props) {
       .catch((err) => console.error(err));
   }, []);
 
+  if (!user || !form) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ActivityIndicator
+          animating
+          color="#000"
+          size="large"
+        />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          alignItems: 'center',
+          flexDirection: 'row',
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <MaterialIcons
+            name="close"
+            size={24}
+            color="#000"
+            onPress={() => navigation.goBack()}
+            style={{ padding: 16 }}
+          />
+        </View>
+
+        <Text
+          style={{
+            fontSize: 16,
+            fontWeight: 'bold',
+            textAlign: 'center',
+          }}
+        >
+          Modifier le profil
+        </Text>
+
+        <View style={{ flex: 1 }}>
+          <Text
+            onPress={() => { }}
+            style={{
+              padding: 16,
+              textAlign: 'right',
+            }}
+          >
+            Enregistrer
+          </Text>
+        </View>
+      </View>
+
+      <ScrollView
+        contentContainerStyle={{
+          padding: 16,
+        }}
+      >
+        <View
+          style={{
+            alignItems: 'center',
+            marginBottom: 16,
+          }}
+        >
+          <Image
+            source={{ uri: form.avatar ?? undefined }}
+            style={styles.avatar}
+          />
+        </View>
+
+        <View style={styles.row}>
+          <Text style={styles.label}>
+            Pseudo
+          </Text>
+
+          <TextInput
+            value={form.pseudo}
+            onChangeText={(text) => setForm((prev) => ({ ...prev, pseudo: text }))}
+            placeholder="Votre pseudo"
+            placeholderTextColor="#666"
+            style={styles.input}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+  },
+  avatar: {
+    width: 100,
+    aspectRatio: 1 / 1,
+    backgroundColor: '#ccc',
+    borderRadius: 360,
+  },
+  row: {
+    alignItems: 'center',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    paddingVertical: 16,
+  },
+  label: {
+    width: 100,
+    fontWeight: 'bold',
+  },
+  input: {
+    flex: 1,
+    padding: 0,
+  },
 });
