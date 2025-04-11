65079480247ab49216845a80bb9105b3d7991748
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-11 19:05:28 +0200

    feat(profile-edit): retrieve user data

--- screens/profile-edit/ProfileEditScreen.tsx
+++ screens/profile-edit/ProfileEditScreen.tsx
@@ -1,13 +1,30 @@
 import { StaticScreenProps } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { User } from '../../models';
+import { IUser } from '../../models/user.model';
 
 type Props = StaticScreenProps<{
   id: string;
 }>;
 
 export default function ProfileEditScreen({ route }: Props) {
+  const [user, setUser] = useState<User>();
+  const [form, setForm] = useState<IUser>(undefined as any);
+
+  useEffect(() => {
+    const prepare = async () => {
+      const user = await User.findById(route.params.id);
+
+      setUser(user);
+      setForm(user.toObject());
+    };
+
+    prepare()
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
     </SafeAreaView>
