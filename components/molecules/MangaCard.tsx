3d06659056c464bf964f8d5e4d16a96be6db3ab2
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-16 19:16:39 +0200

    feat(manga-card): implement update manga entry

--- components/molecules/MangaCard.tsx
+++ components/molecules/MangaCard.tsx
@@ -1,23 +1,103 @@
-import React from 'react';
-import { Image, Pressable, PressableProps, StyleSheet, Text, ViewStyle } from 'react-native';
-import { Manga } from '../../models';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import React, { useContext, useState } from 'react';
+import { ActivityIndicator, Image, Pressable, PressableProps, StyleSheet, Text, View, ViewStyle } from 'react-native';
+import { AuthContext } from '../../contexts/AuthContext';
+import { Manga, MangaEntry, User } from '../../models';
 
 type Props = PressableProps & {
   manga: Manga;
+  onMangaChange?: (manga: Manga) => void;
   style?: ViewStyle;
 }
 
-export default function MangaCard({ manga, style, ...props }: Props) {
+export default function MangaCard({
+  manga,
+  onMangaChange = () => { },
+  style,
+  ...props
+}: Props) {
+  const { user } = useContext(AuthContext);
+  const [isUpdating, setIsUpdating] = useState(false);
+
+  const isAdd = manga['manga-entry']?.isAdd ?? false;
+
   return (
     <Pressable
       {...props}
       style={[styles.container, style]}
     >
-      <Image
-        source={{ uri: manga.poster ?? undefined }}
-        resizeMode="cover"
-        style={styles.image}
-      />
+      <View>
+        <Image
+          source={{ uri: manga.poster ?? undefined }}
+          resizeMode="cover"
+          style={styles.image}
+        />
+
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            margin: 4,
+          }}
+        >
+          {user ? (
+            <View
+              style={{
+                backgroundColor: !isAdd ? '#e5e5e5' : '#4281f5',
+                borderRadius: 360,
+                padding: 8,
+              }}
+            >
+              {!isUpdating ? (
+                <MaterialIcons
+                  name="check"
+                  size={20}
+                  color={!isAdd ? '#7e7e7e' : '#fff'}
+                  onPress={() => {
+                    setIsUpdating(true);
+
+                    const updateMangaEntry = async () => {
+                      if (manga['manga-entry']) {
+                        const mangaEntry = manga['manga-entry'].copy({
+                          isAdd: !isAdd,
+                        });
+                        await mangaEntry.save();
+
+                        onMangaChange(manga.copy({
+                          'manga-entry': mangaEntry,
+                        }));
+                      } else {
+                        const mangaEntry = new MangaEntry({
+                          isAdd: !isAdd,
+
+                          user: new User({ id: user.id }),
+                          manga: manga,
+                        });
+                        await mangaEntry.save();
+
+                        onMangaChange(manga.copy({
+                          'manga-entry': mangaEntry,
+                        }));
+                      }
+                    };
+
+                    updateMangaEntry()
+                      .catch((err) => console.error(err))
+                      .finally(() => setIsUpdating(false));
+                  }}
+                />
+              ) : (
+                <ActivityIndicator
+                  animating
+                  color="#fff"
+                  size={20}
+                />
+              )}
+            </View>
+          ) : null}
+        </View>
+      </View>
 
       <Text
         numberOfLines={2}
