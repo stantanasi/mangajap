dfb60360ab0ebd7941cc5acc93e438aea3e26209
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-17 11:23:46 +0200

    feat(anime): improve displaying anime with tab bar

--- screens/anime/AnimeScreen.tsx
+++ screens/anime/AnimeScreen.tsx
@@ -1,17 +1,26 @@
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { StaticScreenProps, useNavigation } from '@react-navigation/native';
 import React, { useContext, useEffect, useState } from 'react';
-import { ActivityIndicator, Modal, Pressable, SectionList, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Modal, Pressable, ScrollView, SectionList, StyleProp, StyleSheet, Text, View, ViewStyle } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AutoHeightImage from '../../components/atoms/AutoHeightImage';
+import TabBar from '../../components/atoms/TabBar';
 import EpisodeCard from '../../components/molecules/EpisodeCard';
 import SeasonCard from '../../components/molecules/SeasonCard';
 import { AuthContext } from '../../contexts/AuthContext';
 import { Anime, AnimeEntry, Episode, EpisodeEntry, Season, User } from '../../models';
 
-const Header = ({ anime }: { anime: Anime }) => {
+const AboutTab = ({ anime, style }: {
+  anime: Anime;
+  style?: StyleProp<ViewStyle>;
+}) => {
   return (
-    <View style={styles.header}>
+    <ScrollView
+      contentContainerStyle={{
+        paddingVertical: 16,
+      }}
+      style={style}
+    >
       <AutoHeightImage
         source={{ uri: anime.poster ?? undefined }}
         style={styles.poster}
@@ -46,6 +55,234 @@ const Header = ({ anime }: { anime: Anime }) => {
       <Text style={styles.overview}>
         {anime.overview}
       </Text>
+    </ScrollView>
+  );
+};
+
+const EpisodesTab = ({ anime, onAnimeChange, style }: {
+  anime: Anime;
+  onAnimeChange: (anime: Anime) => void;
+  style?: StyleProp<ViewStyle>;
+}) => {
+  const { user } = useContext(AuthContext);
+  const [expandedSeasons, setExpandedSeasons] = useState<{ [seasonId: string]: boolean }>({});
+  const [updating, setUpdating] = useState<{ [id: string]: boolean }>({});
+  const [previousUnwatched, setPreviousUnwatched] = useState<(Season | Episode)[]>();
+
+  const findPreviousSeasonsEpisodes = (item: Season | Episode) => {
+    const sections = anime.seasons!.map((season) => ({
+      season: season,
+      data: season.episodes!,
+    }));
+
+    const previous: (Season | Episode)[] = [];
+
+    for (const section of sections) {
+      if (section.season.id === item.id) {
+        return previous;
+      }
+
+      for (const episode of section.data) {
+        if (episode.id === item.id) {
+          return previous;
+        }
+        previous.push(episode);
+      }
+
+      previous.push(section.season);
+    }
+
+    return previous;
+  };
+
+  return (
+    <View style={style}>
+      <SectionList
+        sections={anime.seasons!.map((season) => ({
+          season: season,
+          data: expandedSeasons[season.id] ? season.episodes! : [],
+        }))}
+        keyExtractor={(item) => item.id}
+        renderSectionHeader={({ section: { season } }) => (
+          <SeasonCard
+            season={season}
+            onSeasonChange={(season) => {
+              onAnimeChange(anime.copy({
+                seasons: anime.seasons?.map((s) => s.id === season.id ? season : s),
+              }));
+            }}
+            onWatchedChange={(value) => {
+              if (!value) return
+
+              const previousUnwatched = findPreviousSeasonsEpisodes(season)
+                .filter((value) => value instanceof Season
+                  ? value.episodes!.some((e) => !e['episode-entry'])
+                  : !value['episode-entry']
+                );
+
+              if (previousUnwatched.length > 0) {
+                setPreviousUnwatched(previousUnwatched);
+              }
+            }}
+            updating={updating[season.id]}
+            onUpdatingChange={(value) => setUpdating((prev) => ({
+              ...prev,
+              [season.id]: value,
+              ...season.episodes?.reduce((acc, episode) => {
+                acc[episode.id] = value;
+                return acc;
+              }, {} as typeof updating),
+            }))}
+            onEpisodeUpdatingChange={(id, value) => setUpdating((prev) => ({ ...prev, [id]: value }))}
+            onPress={() => setExpandedSeasons((prev) => ({ ...prev, [season.id]: !prev[season.id] }))}
+            expanded={expandedSeasons[season.id]}
+            style={{
+              marginTop: 5,
+              marginHorizontal: 16,
+            }}
+          />
+        )}
+        renderSectionFooter={() => <View style={{ height: 5 }} />}
+        renderItem={({ item, section: { season } }) => (
+          <EpisodeCard
+            episode={item}
+            onEpisodeChange={(episode) => {
+              onAnimeChange(anime.copy({
+                seasons: anime.seasons?.map((s) => s.id === season.id
+                  ? season.copy({
+                    episodes: season.episodes?.map((e) => e.id === episode.id ? episode : e),
+                  })
+                  : s),
+              }));
+            }}
+            onWatchedChange={(value) => {
+              if (!value) return
+
+              const previousUnwatched = findPreviousSeasonsEpisodes(item)
+                .filter((value) => value instanceof Season
+                  ? value.episodes!.some((e) => !e['episode-entry'])
+                  : !value['episode-entry']
+                );
+
+              if (previousUnwatched.length > 0) {
+                setPreviousUnwatched(previousUnwatched);
+              }
+            }}
+            updating={updating[item.id]}
+            onUpdatingChange={(value) => setUpdating((prev) => ({ ...prev, [item.id]: value }))}
+            style={{
+              marginHorizontal: 16,
+            }}
+          />
+        )}
+        SectionSeparatorComponent={() => <View style={{ height: 10 }} />}
+        ItemSeparatorComponent={() => <View style={{ height: 6 }} />}
+        contentContainerStyle={{
+          paddingVertical: 11,
+        }}
+      />
+
+      <Modal
+        animationType="fade"
+        onRequestClose={() => setPreviousUnwatched(undefined)}
+        transparent
+        visible={!!previousUnwatched}
+      >
+        <Pressable
+          onPress={() => setPreviousUnwatched(undefined)}
+          style={{
+            alignItems: 'center',
+            backgroundColor: '#00000052',
+            flex: 1,
+            justifyContent: 'center',
+          }}
+        >
+          <Pressable
+            style={{
+              width: '90%',
+              backgroundColor: '#fff',
+              borderRadius: 4,
+              padding: 16,
+              gap: 12,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: 'bold',
+              }}
+            >
+              Marquer les épisodes précédents ?
+            </Text>
+
+            <Text>
+              Voulez-vous marquer les épisodes précédents comme vus ?
+            </Text>
+
+            <View style={{ alignSelf: 'flex-end', flexDirection: 'row', gap: 16 }}>
+              <Text
+                onPress={() => {
+                  setUpdating((prev) => ({
+                    ...prev,
+                    ...Object.fromEntries(previousUnwatched!.map((value) => [value.id, true])),
+                  }));
+
+                  Promise.all(previousUnwatched!.map(async (value) => {
+                    if (value instanceof Episode) {
+                      let episode = value;
+
+                      const episodeEntry = new EpisodeEntry({
+                        user: new User({ id: user!.id }),
+                        episode: episode,
+                      });
+
+                      episode = await episodeEntry.save()
+                        .then((entry) => episode.copy({ 'episode-entry': entry }))
+                        .catch((err) => {
+                          console.error(err);
+                          return episode;
+                        });
+
+                      const season = anime.seasons!.find((season) => season.episodes!.some((e) => e.id === episode.id))!;
+
+                      onAnimeChange(anime.copy({
+                        seasons: anime.seasons?.map((s) => s.id === season.id
+                          ? s.copy({
+                            episodes: s.episodes?.map((e) => e.id === episode.id ? episode : e),
+                          })
+                          : s),
+                      }));
+                      setUpdating((prev) => ({ ...prev, [episode.id]: false }));
+                    }
+                  }))
+                    .catch((err) => console.error(err))
+                    .finally(() => setUpdating((prev) => ({
+                      ...prev,
+                      ...Object.fromEntries(previousUnwatched!.map((value) => [value.id, false])),
+                    })));
+
+                  setPreviousUnwatched(undefined);
+                }}
+                style={{
+                  fontWeight: 'bold',
+                  padding: 10,
+                }}
+              >
+                Oui
+              </Text>
+
+              <Text
+                onPress={() => setPreviousUnwatched(undefined)}
+                style={{
+                  padding: 10,
+                }}
+              >
+                Non
+              </Text>
+            </View>
+          </Pressable>
+        </Pressable>
+      </Modal>
     </View>
   );
 };
@@ -59,10 +296,8 @@ export default function AnimeScreen({ route }: Props) {
   const navigation = useNavigation();
   const { isAuthenticated, user } = useContext(AuthContext);
   const [anime, setAnime] = useState<Anime>();
+  const [selectedTab, setSelectedTab] = useState<'about' | 'episodes'>('about');
   const [isUpdating, setIsUpdating] = useState(false);
-  const [expandedSeasons, setExpandedSeasons] = useState<{ [seasonId: string]: boolean }>({});
-  const [updating, setUpdating] = useState<{ [id: string]: boolean }>({});
-  const [previousUnwatched, setPreviousUnwatched] = useState<(Season | Episode)[]>();
 
   useEffect(() => {
     const prepare = async () => {
@@ -102,116 +337,32 @@ export default function AnimeScreen({ route }: Props) {
     );
   }
 
-  const findPreviousSeasonsEpisodes = (item: Season | Episode) => {
-    const sections = anime.seasons!.map((season) => ({
-      season: season,
-      data: season.episodes!,
-    }));
-
-    const previous: (Season | Episode)[] = [];
-
-    for (const section of sections) {
-      if (section.season.id === item.id) {
-        return previous;
-      }
-
-      for (const episode of section.data) {
-        if (episode.id === item.id) {
-          return previous;
-        }
-        previous.push(episode);
-      }
-
-      previous.push(section.season);
-    }
-
-    return previous;
-  };
-
   return (
     <SafeAreaView style={styles.container}>
-      <SectionList
-        sections={anime.seasons!.map((season) => ({
-          season: season,
-          data: expandedSeasons[season.id] ? season.episodes! : [],
-        }))}
-        keyExtractor={(item) => item.id}
-        renderSectionHeader={({ section: { season } }) => (
-          <SeasonCard
-            season={season}
-            onSeasonChange={(season) => {
-              setAnime((prev) => prev?.copy({
-                seasons: prev.seasons?.map((s) => s.id === season.id ? season : s),
-              }));
-            }}
-            onWatchedChange={(value) => {
-              if (!value) return
+      <TabBar
+        selected={selectedTab}
+        tabs={[
+          { key: 'about', title: 'À propos' },
+          { key: 'episodes', title: 'Épisodes' },
+        ]}
+        onTabChange={(key) => setSelectedTab(key)}
+      />
 
-              const previousUnwatched = findPreviousSeasonsEpisodes(season)
-                .filter((value) => value instanceof Season
-                  ? value.episodes!.some((e) => !e['episode-entry'])
-                  : !value['episode-entry']
-                );
+      <AboutTab
+        anime={anime}
+        style={{
+          display: selectedTab === 'about' ? 'flex' : 'none',
+          flex: 1,
+        }}
+      />
 
-              if (previousUnwatched.length > 0) {
-                setPreviousUnwatched(previousUnwatched);
-              }
-            }}
-            updating={updating[season.id]}
-            onUpdatingChange={(value) => setUpdating((prev) => ({
-              ...prev,
-              [season.id]: value,
-              ...season.episodes?.reduce((acc, episode) => {
-                acc[episode.id] = value;
-                return acc;
-              }, {} as typeof updating),
-            }))}
-            onEpisodeUpdatingChange={(id, value) => setUpdating((prev) => ({ ...prev, [id]: value }))}
-            onPress={() => setExpandedSeasons((prev) => ({ ...prev, [season.id]: !prev[season.id] }))}
-            expanded={expandedSeasons[season.id]}
-            style={{
-              marginHorizontal: 16,
-            }}
-          />
-        )}
-        renderSectionFooter={() => <View style={{ height: 10 }} />}
-        renderItem={({ item, section: { season } }) => (
-          <EpisodeCard
-            episode={item}
-            onEpisodeChange={(episode) => {
-              setAnime((prev) => prev?.copy({
-                seasons: prev.seasons?.map((s) => s.id === season.id
-                  ? season.copy({
-                    episodes: season.episodes?.map((e) => e.id === episode.id ? episode : e),
-                  })
-                  : s),
-              }));
-            }}
-            onWatchedChange={(value) => {
-              if (!value) return
-
-              const previousUnwatched = findPreviousSeasonsEpisodes(item)
-                .filter((value) => value instanceof Season
-                  ? value.episodes!.some((e) => !e['episode-entry'])
-                  : !value['episode-entry']
-                );
-
-              if (previousUnwatched.length > 0) {
-                setPreviousUnwatched(previousUnwatched);
-              }
-            }}
-            updating={updating[item.id]}
-            onUpdatingChange={(value) => setUpdating((prev) => ({ ...prev, [item.id]: value }))}
-            style={{
-              marginHorizontal: 16,
-            }}
-          />
-        )}
-        ListHeaderComponent={Header({
-          anime: anime,
-        })}
-        SectionSeparatorComponent={() => <View style={{ height: 10 }} />}
-        ItemSeparatorComponent={() => <View style={{ height: 6 }} />}
+      <EpisodesTab
+        anime={anime}
+        onAnimeChange={(anime) => setAnime(anime)}
+        style={{
+          display: selectedTab === 'episodes' ? 'flex' : 'none',
+          flex: 1,
+        }}
       />
 
       {user && !anime['anime-entry']?.isAdd ? (
@@ -283,108 +434,6 @@ export default function AnimeScreen({ route }: Props) {
           </Text>
         </Pressable>
       ) : null}
-
-      <Modal
-        animationType="fade"
-        onRequestClose={() => setPreviousUnwatched(undefined)}
-        transparent
-        visible={!!previousUnwatched}
-      >
-        <Pressable
-          onPress={() => setPreviousUnwatched(undefined)}
-          style={{
-            alignItems: 'center',
-            backgroundColor: '#00000052',
-            flex: 1,
-            justifyContent: 'center',
-          }}
-        >
-          <Pressable
-            style={{
-              width: '90%',
-              backgroundColor: '#fff',
-              borderRadius: 4,
-              padding: 16,
-              gap: 12,
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 18,
-                fontWeight: 'bold',
-              }}
-            >
-              Marquer les épisodes précédents ?
-            </Text>
-
-            <Text>
-              Voulez-vous marquer les épisodes précédents comme vus ?
-            </Text>
-
-            <View style={{ alignSelf: 'flex-end', flexDirection: 'row', gap: 16 }}>
-              <Text
-                onPress={() => {
-                  setUpdating((prev) => ({
-                    ...prev,
-                    ...Object.fromEntries(previousUnwatched!.map((value) => [value.id, true])),
-                  }));
-
-                  Promise.all(previousUnwatched!.map(async (value) => {
-                    if (value instanceof Episode) {
-                      let episode = value;
-
-                      const episodeEntry = new EpisodeEntry({
-                        user: new User({ id: user!.id }),
-                        episode: episode,
-                      });
-
-                      episode = await episodeEntry.save()
-                        .then((entry) => episode.copy({ 'episode-entry': entry }))
-                        .catch((err) => {
-                          console.error(err);
-                          return episode;
-                        });
-
-                      const season = anime.seasons!.find((season) => season.episodes!.some((e) => e.id === episode.id))!;
-
-                      setAnime((prev) => prev?.copy({
-                        seasons: prev.seasons?.map((s) => s.id === season.id
-                          ? s.copy({
-                            episodes: s.episodes?.map((e) => e.id === episode.id ? episode : e),
-                          })
-                          : s),
-                      }));
-                      setUpdating((prev) => ({ ...prev, [episode.id]: false }));
-                    }
-                  }))
-                    .catch((err) => console.error(err))
-                    .finally(() => setUpdating((prev) => ({
-                      ...prev,
-                      ...Object.fromEntries(previousUnwatched!.map((value) => [value.id, false])),
-                    })));
-
-                  setPreviousUnwatched(undefined);
-                }}
-                style={{
-                  fontWeight: 'bold',
-                  padding: 10,
-                }}
-              >
-                Oui
-              </Text>
-
-              <Text
-                onPress={() => setPreviousUnwatched(undefined)}
-                style={{
-                  padding: 10,
-                }}
-              >
-                Non
-              </Text>
-            </View>
-          </Pressable>
-        </Pressable>
-      </Modal>
     </SafeAreaView>
   );
 }
@@ -392,9 +441,6 @@ export default function AnimeScreen({ route }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  header: {
-    marginBottom: 16,
   },
   poster: {
     width: '80%',
