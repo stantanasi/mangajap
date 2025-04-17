aac234d97373f93afff64eeaf34a097392742629
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-17 19:51:09 +0200

    feat(manga-save): retrieve manga data

--- screens/manga-save/MangaSaveScreen.tsx
+++ screens/manga-save/MangaSaveScreen.tsx
@@ -1,13 +1,39 @@
-import { StaticScreenProps } from '@react-navigation/native';
-import React from 'react';
+import { StaticScreenProps, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Manga } from '../../models';
+import { IManga } from '../../models/manga.model';
 
 type Props = StaticScreenProps<{
   id: string
 } | undefined>
 
 export default function MangaSaveScreen({ route }: Props) {
+  const navigation = useNavigation();
+  const [manga, setManga] = useState<Manga>();
+  const [form, setForm] = useState<Partial<IManga>>();
+
+  useEffect(() => {
+    const prepare = async () => {
+      let manga = new Manga({});
+
+      if (route.params) {
+        manga = await Manga.findById(route.params.id);
+      }
+
+      setManga(manga);
+      setForm(manga.toObject());
+    };
+
+    const unsubscribe = navigation.addListener('focus', () => {
+      prepare()
+        .catch((err) => console.error(err));
+    });
+
+    return unsubscribe;
+  }, [route.params]);
+
   return (
     <SafeAreaView style={styles.container}>
     </SafeAreaView>
