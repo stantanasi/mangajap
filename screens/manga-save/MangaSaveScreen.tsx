dc9371f4bab413fbe92db64467547c85bebc72f8
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-17 20:15:23 +0200

    feat(manga-save): display some text input

--- screens/manga-save/MangaSaveScreen.tsx
+++ screens/manga-save/MangaSaveScreen.tsx
@@ -1,7 +1,8 @@
 import { StaticScreenProps, useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import TextInput from '../../components/atoms/TextInput';
 import { Manga } from '../../models';
 import { IManga } from '../../models/manga.model';
 
@@ -34,8 +35,57 @@ export default function MangaSaveScreen({ route }: Props) {
     return unsubscribe;
   }, [route.params]);
 
+  if (!manga || !form) {
+    return (
+      <SafeAreaView style={[styles.container, { alignItems: 'center', justifyContent: 'center' }]}>
+        <ActivityIndicator
+          animating
+          color="#000"
+          size="large"
+        />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      <TextInput
+        label="Titre"
+        value={form.title}
+        onChangeText={(text) => setForm((prev) => ({
+          ...prev!,
+          title: text,
+        }))}
+        style={styles.input}
+      />
+
+      <TextInput
+        label="Synopsis"
+        value={form.overview}
+        onChangeText={(text) => setForm((prev) => ({
+          ...prev!,
+          overview: text,
+        }))}
+        multiline
+        style={styles.input}
+      />
+
+      <Text style={styles.sectionTitle}>
+        Identifiants externes
+      </Text>
+
+      <TextInput
+        label="Mangadex"
+        value={form.links?.['mangadex']}
+        onChangeText={(text) => setForm((prev) => ({
+          ...prev!,
+          links: {
+            ...prev!.links,
+            mangadex: text,
+          },
+        }))}
+        style={styles.input}
+      />
     </SafeAreaView>
   );
 }
@@ -43,5 +93,15 @@ export default function MangaSaveScreen({ route }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  input: {
+    marginHorizontal: 16,
+    marginTop: 16,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginHorizontal: 16,
+    marginTop: 20,
   },
 });
