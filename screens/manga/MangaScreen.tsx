8db71372041139c18e0a2e83f567ff9817a49080
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-17 11:24:04 +0200

    feat(manga): improve displaying manga with tab bar

--- screens/manga/MangaScreen.tsx
+++ screens/manga/MangaScreen.tsx
@@ -1,17 +1,26 @@
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { StaticScreenProps, useNavigation } from '@react-navigation/native';
 import React, { useContext, useEffect, useState } from 'react';
-import { ActivityIndicator, Modal, Pressable, SectionList, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Modal, Pressable, ScrollView, SectionList, StyleProp, StyleSheet, Text, View, ViewStyle } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AutoHeightImage from '../../components/atoms/AutoHeightImage';
+import TabBar from '../../components/atoms/TabBar';
 import ChapterCard from '../../components/molecules/ChapterCard';
 import VolumeCard from '../../components/molecules/VolumeCard';
 import { AuthContext } from '../../contexts/AuthContext';
 import { Chapter, ChapterEntry, Manga, MangaEntry, User, Volume, VolumeEntry } from '../../models';
 
-const Header = ({ manga }: { manga: Manga }) => {
+const AboutTab = ({ manga, style }: {
+  manga: Manga;
+  style?: StyleProp<ViewStyle>;
+}) => {
   return (
-    <View style={styles.header}>
+    <ScrollView
+      contentContainerStyle={{
+        paddingVertical: 16,
+      }}
+      style={style}
+    >
       <AutoHeightImage
         source={{ uri: manga.poster ?? undefined }}
         style={styles.poster}
@@ -46,58 +55,19 @@ const Header = ({ manga }: { manga: Manga }) => {
       <Text style={styles.overview}>
         {manga.overview}
       </Text>
-    </View>
+    </ScrollView>
   );
 };
 
-
-type Props = StaticScreenProps<{
-  id: string;
-}>;
-
-export default function MangaScreen({ route }: Props) {
-  const navigation = useNavigation();
-  const { isAuthenticated, user } = useContext(AuthContext);
-  const [manga, setManga] = useState<Manga>();
-  const [isUpdating, setIsUpdating] = useState(false);
+const VolumesTab = ({ manga, onMangaChange, style }: {
+  manga: Manga;
+  onMangaChange: (manga: Manga) => void;
+  style?: StyleProp<ViewStyle>;
+}) => {
+  const { user } = useContext(AuthContext);
   const [expandedVolumes, setExpandedVolumes] = useState<{ [volumeId: string]: boolean }>({});
   const [updating, setUpdating] = useState<{ [id: string]: boolean }>({});
   const [previousUnread, setPreviousUnread] = useState<(Volume | Chapter)[]>();
-
-  useEffect(() => {
-    const prepare = async () => {
-      const manga = await Manga.findById(route.params.id)
-        .include([
-          'genres',
-          'themes',
-          `volumes${isAuthenticated ? '.volume-entry' : ''}`,
-          `volumes.chapters${isAuthenticated ? '.chapter-entry' : ''}`,
-          `chapters${isAuthenticated ? '.chapter-entry' : ''}`,
-          isAuthenticated ? 'manga-entry' : '',
-        ]);
-
-      setManga(manga);
-    };
-
-    const unsubscribe = navigation.addListener('focus', () => {
-      prepare()
-        .catch((err) => console.error(err));
-    });
-
-    return unsubscribe;
-  }, [route.params]);
-
-  if (!manga) {
-    return (
-      <SafeAreaView style={[styles.container, { alignItems: 'center', justifyContent: 'center' }]}>
-        <ActivityIndicator
-          animating
-          color="#000"
-          size="large"
-        />
-      </SafeAreaView>
-    );
-  }
 
   const findPreviousVolumesChapters = (item: Volume | Chapter): (Volume | Chapter)[] => {
     const sections = [
@@ -134,7 +104,7 @@ export default function MangaScreen({ route }: Props) {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={style}>
       <SectionList
         sections={[
           ...manga.volumes!.map((volume) => ({
@@ -151,8 +121,8 @@ export default function MangaScreen({ route }: Props) {
           <VolumeCard
             volume={volume}
             onVolumeChange={(volume) => {
-              setManga((prev) => prev?.copy({
-                volumes: prev.volumes?.map((v) => v.id === volume.id ? volume : v),
+              onMangaChange(manga.copy({
+                volumes: manga.volumes?.map((v) => v.id === volume.id ? volume : v),
               }));
             }}
             onReadChange={(value) => {
@@ -174,18 +144,19 @@ export default function MangaScreen({ route }: Props) {
             onPress={() => setExpandedVolumes((prev) => ({ ...prev, [volume.id]: !prev[volume.id] }))}
             expanded={expandedVolumes[volume.id]}
             style={{
+              marginTop: 5,
               marginHorizontal: 16,
             }}
           />
         )}
-        renderSectionFooter={() => <View style={{ height: 10 }} />}
+        renderSectionFooter={() => <View style={{ height: 5 }} />}
         renderItem={({ item, section: { volume } }) => (
           <ChapterCard
             chapter={item}
             onChapterChange={(chapter) => {
               if (volume) {
-                setManga((prev) => prev?.copy({
-                  volumes: prev.volumes?.map((v) => v.id === volume.id
+                onMangaChange(manga.copy({
+                  volumes: manga.volumes?.map((v) => v.id === volume.id
                     ? volume.copy({
                       chapters: volume.chapters?.map((c) => c.id === chapter.id ? chapter : c)
                     })
@@ -193,8 +164,8 @@ export default function MangaScreen({ route }: Props) {
                   ),
                 }));
               } else {
-                setManga((prev) => prev?.copy({
-                  chapters: prev.chapters?.map((c) => c.id === chapter.id ? chapter : c),
+                onMangaChange(manga.copy({
+                  chapters: manga.chapters?.map((c) => c.id === chapter.id ? chapter : c),
                 }));
               }
             }}
@@ -218,11 +189,212 @@ export default function MangaScreen({ route }: Props) {
             }}
           />
         )}
-        ListHeaderComponent={Header({
-          manga: manga,
-        })}
         SectionSeparatorComponent={() => <View style={{ height: 10 }} />}
         ItemSeparatorComponent={() => <View style={{ height: 6 }} />}
+        contentContainerStyle={{
+          paddingVertical: 11,
+        }}
+      />
+
+      <Modal
+        animationType="fade"
+        onRequestClose={() => setPreviousUnread(undefined)}
+        transparent
+        visible={!!previousUnread}
+      >
+        <Pressable
+          onPress={() => setPreviousUnread(undefined)}
+          style={{
+            alignItems: 'center',
+            backgroundColor: '#00000052',
+            flex: 1,
+            justifyContent: 'center',
+          }}
+        >
+          <Pressable
+            style={{
+              width: '90%',
+              backgroundColor: '#fff',
+              borderRadius: 4,
+              padding: 16,
+              gap: 12,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: 'bold',
+              }}
+            >
+              Marquer les volumes et chapitres précédents ?
+            </Text>
+
+            <Text>
+              Voulez-vous marquer les volumes et chapitres précédents comme lus ?
+            </Text>
+
+            <View style={{ alignSelf: 'flex-end', flexDirection: 'row', gap: 16 }}>
+              <Text
+                onPress={() => {
+                  setUpdating((prev) => ({
+                    ...prev,
+                    ...Object.fromEntries(previousUnread!.map((value) => [value.id, true])),
+                  }));
+
+                  Promise.all(previousUnread!.map(async (value) => {
+                    if (value instanceof Volume) {
+                      let volume = value;
+
+                      const volumeEntry = new VolumeEntry({
+                        user: new User({ id: user!.id }),
+                        volume: volume,
+                      });
+
+                      volume = await volumeEntry.save()
+                        .then((entry) => volume.copy({ 'volume-entry': entry }))
+                        .catch((err) => {
+                          console.error(err);
+                          return volume;
+                        });;
+
+                      onMangaChange(manga.copy({
+                        volumes: manga.volumes?.map((v) => v.id === volume.id ? volume : v),
+                      }));
+                    } else {
+                      let chapter = value;
+
+                      const chapterEntry = new ChapterEntry({
+                        user: new User({ id: user!.id }),
+                        chapter: chapter,
+                      });
+
+                      chapter = await chapterEntry.save()
+                        .then((entry) => chapter.copy({ 'chapter-entry': entry }))
+                        .catch((err) => {
+                          console.error(err);
+                          return chapter;
+                        });
+
+                      const volume = manga.volumes!.find((volume) => volume.chapters!.some((c) => c.id === chapter.id));
+                      if (volume) {
+                        onMangaChange(manga.copy({
+                          volumes: manga.volumes?.map((v) => v.id === volume.id
+                            ? volume.copy({
+                              chapters: volume.chapters?.map((c) => c.id === chapter.id ? chapter : c)
+                            })
+                            : v,
+                          ),
+                        }));
+                      } else {
+                        onMangaChange(manga.copy({
+                          chapters: manga.chapters?.map((c) => c.id === chapter.id ? chapter : c),
+                        }));
+                      }
+                    }
+
+                    setUpdating((prev) => ({ ...prev, [value.id]: false }));
+                  }))
+                    .catch((err) => console.error(err));
+
+                  setPreviousUnread(undefined);
+                }}
+                style={{
+                  fontWeight: 'bold',
+                  padding: 10,
+                }}
+              >
+                Oui
+              </Text>
+
+              <Text
+                onPress={() => setPreviousUnread(undefined)}
+                style={{
+                  padding: 10,
+                }}
+              >
+                Non
+              </Text>
+            </View>
+          </Pressable>
+        </Pressable>
+      </Modal>
+    </View>
+  );
+};
+
+
+type Props = StaticScreenProps<{
+  id: string;
+}>;
+
+export default function MangaScreen({ route }: Props) {
+  const navigation = useNavigation();
+  const { isAuthenticated, user } = useContext(AuthContext);
+  const [manga, setManga] = useState<Manga>();
+  const [selectedTab, setSelectedTab] = useState<'about' | 'volumes'>('about');
+  const [isUpdating, setIsUpdating] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      const manga = await Manga.findById(route.params.id)
+        .include([
+          'genres',
+          'themes',
+          `volumes${isAuthenticated ? '.volume-entry' : ''}`,
+          `volumes.chapters${isAuthenticated ? '.chapter-entry' : ''}`,
+          `chapters${isAuthenticated ? '.chapter-entry' : ''}`,
+          isAuthenticated ? 'manga-entry' : '',
+        ]);
+
+      setManga(manga);
+    };
+
+    const unsubscribe = navigation.addListener('focus', () => {
+      prepare()
+        .catch((err) => console.error(err));
+    });
+
+    return unsubscribe;
+  }, [route.params]);
+
+  if (!manga) {
+    return (
+      <SafeAreaView style={[styles.container, { alignItems: 'center', justifyContent: 'center' }]}>
+        <ActivityIndicator
+          animating
+          color="#000"
+          size="large"
+        />
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TabBar
+        selected={selectedTab}
+        tabs={[
+          { key: 'about', title: 'À propos' },
+          { key: 'volumes', title: 'Tomes' },
+        ]}
+        onTabChange={(key) => setSelectedTab(key)}
+      />
+
+      <AboutTab
+        manga={manga}
+        style={{
+          display: selectedTab === 'about' ? 'flex' : 'none',
+          flex: 1,
+        }}
+      />
+
+      <VolumesTab
+        manga={manga}
+        onMangaChange={(manga) => setManga(manga)}
+        style={{
+          display: selectedTab === 'volumes' ? 'flex' : 'none',
+          flex: 1,
+        }}
       />
 
       {user && !manga['manga-entry']?.isAdd ? (
@@ -294,129 +466,6 @@ export default function MangaScreen({ route }: Props) {
           </Text>
         </Pressable>
       ) : null}
-
-      <Modal
-        animationType="fade"
-        onRequestClose={() => setPreviousUnread(undefined)}
-        transparent
-        visible={!!previousUnread}
-      >
-        <Pressable
-          onPress={() => setPreviousUnread(undefined)}
-          style={{
-            alignItems: 'center',
-            backgroundColor: '#00000052',
-            flex: 1,
-            justifyContent: 'center',
-          }}
-        >
-          <Pressable
-            style={{
-              width: '90%',
-              backgroundColor: '#fff',
-              borderRadius: 4,
-              padding: 16,
-              gap: 12,
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 18,
-                fontWeight: 'bold',
-              }}
-            >
-              Marquer les volumes et chapitres précédents ?
-            </Text>
-
-            <Text>
-              Voulez-vous marquer les volumes et chapitres précédents comme lus ?
-            </Text>
-
-            <View style={{ alignSelf: 'flex-end', flexDirection: 'row', gap: 16 }}>
-              <Text
-                onPress={() => {
-                  setUpdating((prev) => ({
-                    ...prev,
-                    ...Object.fromEntries(previousUnread!.map((value) => [value.id, true])),
-                  }));
-
-                  Promise.all(previousUnread!.map(async (value) => {
-                    if (value instanceof Volume) {
-                      let volume = value;
-
-                      const volumeEntry = new VolumeEntry({
-                        user: new User({ id: user!.id }),
-                        volume: volume,
-                      });
-
-                      volume = await volumeEntry.save()
-                        .then((entry) => volume.copy({ 'volume-entry': entry }))
-                        .catch((err) => {
-                          console.error(err);
-                          return volume;
-                        });;
-
-                      setManga((prev) => prev?.copy({
-                        volumes: prev.volumes?.map((v) => v.id === volume.id ? volume : v),
-                      }));
-                    } else {
-                      let chapter = value;
-
-                      const chapterEntry = new ChapterEntry({
-                        user: new User({ id: user!.id }),
-                        chapter: chapter,
-                      });
-
-                      chapter = await chapterEntry.save()
-                        .then((entry) => chapter.copy({ 'chapter-entry': entry }))
-                        .catch((err) => {
-                          console.error(err);
-                          return chapter;
-                        });
-
-                      const volume = manga.volumes!.find((volume) => volume.chapters!.some((c) => c.id === chapter.id));
-                      if (volume) {
-                        setManga((prev) => prev?.copy({
-                          volumes: prev.volumes?.map((v) => v.id === volume.id
-                            ? volume.copy({
-                              chapters: volume.chapters?.map((c) => c.id === chapter.id ? chapter : c)
-                            })
-                            : v,
-                          ),
-                        }));
-                      } else {
-                        setManga((prev) => prev?.copy({
-                          chapters: prev.chapters?.map((c) => c.id === chapter.id ? chapter : c),
-                        }));
-                      }
-                    }
-
-                    setUpdating((prev) => ({ ...prev, [value.id]: false }));
-                  }))
-                    .catch((err) => console.error(err));
-
-                  setPreviousUnread(undefined);
-                }}
-                style={{
-                  fontWeight: 'bold',
-                  padding: 10,
-                }}
-              >
-                Oui
-              </Text>
-
-              <Text
-                onPress={() => setPreviousUnread(undefined)}
-                style={{
-                  padding: 10,
-                }}
-              >
-                Non
-              </Text>
-            </View>
-          </Pressable>
-        </Pressable>
-      </Modal>
     </SafeAreaView>
   );
 }
@@ -424,9 +473,6 @@ export default function MangaScreen({ route }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  header: {
-    marginBottom: 16,
   },
   poster: {
     width: '80%',
