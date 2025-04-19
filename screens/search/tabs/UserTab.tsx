385be95a1efecb70ad5136a672ed0175c03c25d8
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-19 18:47:31 +0200

    refactor(search): move search tabs to dedicated tab files

---
+++ screens/search/tabs/UserTab.tsx
@@ -0,0 +1,74 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { ActivityIndicator, FlatList, StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
+import UserCard from '../../../components/molecules/UserCard';
+import { User } from '../../../models';
+
+type Props = {
+  isLoading: boolean;
+  list: User[];
+  onLoadMore: () => void;
+  hasMore: boolean;
+  style?: StyleProp<ViewStyle>;
+}
+
+export default function UserTab({ isLoading, list, onLoadMore, hasMore, style }: Props) {
+  const navigation = useNavigation();
+
+  if (isLoading) {
+    return (
+      <View style={[styles.container, style, { alignItems: 'center', justifyContent: 'center' }]}>
+        <ActivityIndicator
+          animating
+          color="#000"
+          size="large"
+        />
+      </View>
+    );
+  }
+
+  return (
+    <View style={[styles.container, style]}>
+      <FlatList
+        data={list}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <UserCard
+            user={item}
+            onPress={() => navigation.navigate('Profile', { id: item.id })}
+            style={{
+              marginHorizontal: 16,
+            }}
+          />
+        )}
+        ItemSeparatorComponent={() => (
+          <View
+            style={{
+              width: '100%',
+              height: 1,
+              backgroundColor: '#ccc',
+              marginVertical: 8,
+            }}
+          />
+        )}
+        ListFooterComponent={() => (
+          hasMore ? (
+            <View style={{ marginVertical: 12 }}>
+              <ActivityIndicator
+                animating
+                color="#000"
+              />
+            </View>
+          ) : null
+        )}
+        keyboardShouldPersistTaps="always"
+        onEndReached={() => onLoadMore()}
+        onEndReachedThreshold={0.5}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
