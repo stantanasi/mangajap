9829ef3e158b69de89ea9b5db064b4679cac6c57
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-19 21:37:56 +0200

    feat(chapter-save): retrieve chapter data

--- screens/chapter-save/ChapterSaveScreen.tsx
+++ screens/chapter-save/ChapterSaveScreen.tsx
@@ -1,7 +1,9 @@
-import { StaticScreenProps } from '@react-navigation/native';
-import React from 'react';
+import { StaticScreenProps, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Chapter, Manga } from '../../models';
+import { IChapter } from '../../models/chapter.model';
 
 type Props = StaticScreenProps<{
   mangaId: string;
@@ -10,6 +12,34 @@ type Props = StaticScreenProps<{
 }>
 
 export default function ChapterSaveScreen({ route }: Props) {
+  const navigation = useNavigation();
+  const [chapter, setChapter] = useState<Chapter>();
+  const [form, setForm] = useState<Partial<IChapter>>();
+
+  useEffect(() => {
+    const prepare = async () => {
+      let chapter = new Chapter();
+
+      if ('mangaId' in route.params) {
+        chapter = new Chapter({
+          manga: new Manga({ id: route.params.mangaId }),
+        });
+      } else {
+        chapter = await Chapter.findById(route.params.chapterId);
+      }
+
+      setChapter(chapter);
+      setForm(chapter.toObject());
+    };
+
+    const unsubscribe = navigation.addListener('focus', () => {
+      prepare()
+        .catch((err) => console.error(err));
+    });
+
+    return unsubscribe;
+  }, [route.params]);
+
   return (
     <SafeAreaView style={styles.container}>
     </SafeAreaView>
