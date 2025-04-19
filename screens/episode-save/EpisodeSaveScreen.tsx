19638b045900f87f59b9d085c89a371825c1b526
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-20 00:06:13 +0200

    feat(episode-save): retrieve episode data

--- screens/episode-save/EpisodeSaveScreen.tsx
+++ screens/episode-save/EpisodeSaveScreen.tsx
@@ -1,7 +1,9 @@
-import { StaticScreenProps } from '@react-navigation/native';
-import React from 'react';
+import { StaticScreenProps, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Anime, Episode, Season } from '../../models';
+import { IEpisode } from '../../models/episode.model';
 
 type Props = StaticScreenProps<{
   animeId: string;
@@ -10,6 +12,43 @@ type Props = StaticScreenProps<{
 }>
 
 export default function EpisodeSaveScreen({ route }: Props) {
+  const navigation = useNavigation();
+  const [episode, setEpisode] = useState<Episode>();
+  const [form, setForm] = useState<Partial<IEpisode>>();
+  const [seasons, setSeasons] = useState<Season[]>();
+
+  useEffect(() => {
+    const prepare = async () => {
+      let episode = new Episode();
+
+      if ('animeId' in route.params) {
+        episode = new Episode({
+          anime: new Anime({
+            id: route.params.animeId,
+            seasons: await Anime.findById(route.params.animeId).get('seasons'),
+          }),
+        });
+      } else {
+        episode = await Episode.findById(route.params.episodeId)
+          .include([
+            'anime.seasons',
+            'season',
+          ]);
+      }
+
+      setEpisode(episode);
+      setForm(episode.toObject());
+      setSeasons(episode.anime?.seasons ?? []);
+    };
+
+    const unsubscribe = navigation.addListener('focus', () => {
+      prepare()
+        .catch((err) => console.error(err));
+    });
+
+    return unsubscribe;
+  }, [route.params]);
+
   return (
     <SafeAreaView style={styles.container}>
     </SafeAreaView>
