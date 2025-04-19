b4f86870e82d89fc5b88c2119f532e4bf90a7ab6
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-19 21:40:03 +0200

    feat(volume-save): display volume save form

--- screens/volume-save/VolumeSaveScreen.tsx
+++ screens/volume-save/VolumeSaveScreen.tsx
@@ -1,7 +1,11 @@
 import { StaticScreenProps, useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { ActivityIndicator, ScrollView, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import DateInput from '../../components/atoms/DateInput';
+import ImageInput from '../../components/atoms/ImageInput';
+import NumberInput from '../../components/atoms/NumberInput';
+import TextInput from '../../components/atoms/TextInput';
 import { Manga, Volume } from '../../models';
 import { IVolume } from '../../models/volume.model';
 
@@ -40,8 +44,76 @@ export default function VolumeSaveScreen({ route }: Props) {
     return unsubscribe;
   }, [route.params]);
 
+  if (!volume || !form) {
+    return (
+      <SafeAreaView style={[styles.container, { alignItems: 'center', justifyContent: 'center' }]}>
+        <ActivityIndicator
+          animating
+          color="#000"
+          size="large"
+        />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <ImageInput
+          label="Couverture"
+          value={form.cover}
+          onValueChange={(value) => setForm((prev) => ({
+            ...prev,
+            cover: value,
+          }))}
+          style={styles.input}
+          inputStyle={{
+            width: 150,
+            minHeight: 150 * 3 / 2,
+          }}
+        />
+
+        <NumberInput
+          label="Numéro *"
+          value={form.number}
+          onValueChange={(value) => setForm((prev) => ({
+            ...prev,
+            number: value,
+          }))}
+          style={styles.input}
+        />
+
+        <TextInput
+          label="Titre"
+          value={form.title}
+          onChangeText={(text) => setForm((prev) => ({
+            ...prev,
+            title: text,
+          }))}
+          style={styles.input}
+        />
+
+        <TextInput
+          label="Synopsis"
+          value={form.overview}
+          onChangeText={(text) => setForm((prev) => ({
+            ...prev,
+            overview: text,
+          }))}
+          multiline
+          style={styles.input}
+        />
+
+        <DateInput
+          label="Date de publication"
+          value={form.publishedDate ?? undefined}
+          onValueChange={(value) => setForm((prev) => ({
+            ...prev,
+            publishedDate: value,
+          }))}
+          style={styles.input}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -49,5 +121,9 @@ export default function VolumeSaveScreen({ route }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  input: {
+    marginHorizontal: 16,
+    marginTop: 16,
   },
 });
