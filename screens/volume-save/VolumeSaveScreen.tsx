07395ec5ffb6d17bbd1246c64a23142f7089cba2
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-19 21:11:52 +0200

    feat(volume-save): retrieve volume data

--- screens/volume-save/VolumeSaveScreen.tsx
+++ screens/volume-save/VolumeSaveScreen.tsx
@@ -1,7 +1,9 @@
-import { StaticScreenProps } from '@react-navigation/native';
-import React from 'react';
+import { StaticScreenProps, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Manga, Volume } from '../../models';
+import { IVolume } from '../../models/volume.model';
 
 type Props = StaticScreenProps<{
   mangaId: string;
@@ -10,6 +12,34 @@ type Props = StaticScreenProps<{
 }>
 
 export default function VolumeSaveScreen({ route }: Props) {
+  const navigation = useNavigation();
+  const [volume, setVolume] = useState<Volume>();
+  const [form, setForm] = useState<Partial<IVolume>>();
+
+  useEffect(() => {
+    const prepare = async () => {
+      let volume = new Volume();
+
+      if ('mangaId' in route.params) {
+        volume = new Volume({
+          manga: new Manga({ id: route.params.mangaId }),
+        });
+      } else {
+        volume = await Volume.findById(route.params.volumeId);
+      }
+
+      setVolume(volume);
+      setForm(volume.toObject());
+    };
+
+    const unsubscribe = navigation.addListener('focus', () => {
+      prepare()
+        .catch((err) => console.error(err));
+    });
+
+    return unsubscribe;
+  }, [route.params]);
+
   return (
     <SafeAreaView style={styles.container}>
     </SafeAreaView>
