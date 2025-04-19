b9696a4badfb00ddf4be6f5d0b10f3fb335a58d5
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-19 17:10:05 +0200

    feat(anime-save): retrieve anime data

--- screens/anime-save/AnimeSaveScreen.tsx
+++ screens/anime-save/AnimeSaveScreen.tsx
@@ -1,13 +1,39 @@
-import { StaticScreenProps } from '@react-navigation/native';
-import React from 'react';
+import { StaticScreenProps, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Anime } from '../../models';
+import { IAnime } from '../../models/anime.model';
 
 type Props = StaticScreenProps<{
   id: string;
 } | undefined>
 
 export default function AnimeSaveScreen({ route }: Props) {
+  const navigation = useNavigation();
+  const [anime, setAnime] = useState<Anime>();
+  const [form, setForm] = useState<Partial<IAnime>>();
+
+  useEffect(() => {
+    const prepare = async () => {
+      let anime = new Anime({});
+
+      if (route.params) {
+        anime = await Anime.findById(route.params.id);
+      }
+
+      setAnime(anime);
+      setForm(anime.toObject());
+    };
+
+    const unsubscribe = navigation.addListener('focus', () => {
+      prepare()
+        .catch((err) => console.error(err));
+    });
+
+    return unsubscribe;
+  }, [route.params]);
+
   return (
     <SafeAreaView style={styles.container}>
     </SafeAreaView>
