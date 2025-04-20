de6f4975d2d33e9a36bc068b56c3b886afff9ae1
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-20 13:18:27 +0200

    feat(manga): retrieve manga's franchises

--- screens/manga/MangaScreen.tsx
+++ screens/manga/MangaScreen.tsx
@@ -29,6 +29,7 @@ export default function MangaScreen({ route }: Props) {
           `volumes${isAuthenticated ? '.volume-entry' : ''}`,
           `volumes.chapters${isAuthenticated ? '.chapter-entry' : ''}`,
           `chapters${isAuthenticated ? '.chapter-entry' : ''}`,
+          'franchises.destination',
           ...(isAuthenticated ? ['manga-entry'] : []),
         ]);
 
