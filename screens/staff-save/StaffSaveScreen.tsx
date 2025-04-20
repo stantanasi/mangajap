5fbef4a19d2ad8e778fcc57638b5ad1751913985
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-20 15:43:02 +0200

    feat(staff-save): retrieve staff data

--- screens/staff-save/StaffSaveScreen.tsx
+++ screens/staff-save/StaffSaveScreen.tsx
@@ -1,7 +1,9 @@
-import { StaticScreenProps } from '@react-navigation/native';
-import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StaticScreenProps, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Anime, Manga, Staff } from '../../models';
+import { IStaff } from '../../models/staff.model';
 
 type Props = StaticScreenProps<{
   animeId: string;
@@ -12,6 +14,38 @@ type Props = StaticScreenProps<{
 }>
 
 export default function StaffSaveScreen({ route }: Props) {
+  const navigation = useNavigation();
+  const [staff, setStaff] = useState<Staff>();
+  const [form, setForm] = useState<Partial<IStaff>>();
+
+  useEffect(() => {
+    const prepare = async () => {
+      let staff = new Staff();
+
+      if ('animeId' in route.params) {
+        staff = new Staff({
+          anime: new Anime({ id: route.params.animeId }),
+        });
+      } else if ('mangaId' in route.params) {
+        staff = new Staff({
+          manga: new Manga({ id: route.params.mangaId }),
+        });
+      } else {
+        staff = await Staff.findById(route.params.staffId);
+      }
+
+      setStaff(staff);
+      setForm(staff.toObject());
+    };
+
+    const unsubscribe = navigation.addListener('focus', () => {
+      prepare()
+        .catch((err) => console.error(err));
+    });
+
+    return unsubscribe;
+  }, [route.params]);
+
   return (
     <SafeAreaView style={styles.container}>
     </SafeAreaView>
