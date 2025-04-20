f9294477464b81d8e0ed6e3d12a807e4186e04c6
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-20 15:56:16 +0200

    feat(staff-save): implement change staff people

--- screens/staff-save/StaffSaveScreen.tsx
+++ screens/staff-save/StaffSaveScreen.tsx
@@ -1,10 +1,102 @@
 import { StaticScreenProps, useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, ScrollView, StyleSheet } from 'react-native';
+import { ActivityIndicator, FlatList, Image, Modal, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import InputLabel from '../../components/atoms/InputLabel';
+import SearchBar from '../../components/atoms/SearchBar';
 import SelectInput from '../../components/atoms/SelectInput';
-import { Anime, Manga, Staff } from '../../models';
+import PeopleCard from '../../components/molecules/PeopleCard';
+import { Anime, Manga, People, Staff } from '../../models';
 import { IStaff } from '../../models/staff.model';
+
+const SelectDestinationModal = ({ onSelect, onRequestClose, visible }: {
+  onSelect: (people: People) => void;
+  onRequestClose: () => void;
+  visible: boolean;
+}) => {
+  const [peoples, setPeoples] = useState<People[]>();
+
+  useEffect(() => {
+    setPeoples(undefined);
+  }, [visible]);
+
+  return (
+    <Modal
+      animationType="fade"
+      onRequestClose={onRequestClose}
+      transparent
+      visible={visible}
+    >
+      <Pressable
+        onPress={onRequestClose}
+        style={{
+          alignItems: 'center',
+          backgroundColor: '#00000052',
+          flex: 1,
+          justifyContent: 'center',
+        }}
+      >
+        <Pressable
+          style={{
+            width: '90%',
+            height: '90%',
+            backgroundColor: '#fff',
+            borderRadius: 4,
+            gap: 12,
+          }}
+        >
+          <SearchBar
+            onChangeText={() => {
+              setPeoples(undefined);
+            }}
+            onSearch={(query) => {
+              setPeoples(undefined);
+
+              People.find({ query: query })
+                .then((peoples) => setPeoples(peoples))
+                .catch((err) => console.error(err));
+            }}
+            delay={500}
+            style={{
+              backgroundColor: undefined,
+              borderColor: '#ccc',
+              borderRadius: 4,
+              borderWidth: 1,
+              marginHorizontal: 16,
+              marginTop: 16,
+            }}
+          />
+
+          {!peoples ? (
+            <ActivityIndicator
+              animating
+              color="#000"
+              size="large"
+            />
+          ) : (
+            <FlatList
+              data={peoples}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => (
+                <PeopleCard
+                  people={item}
+                  onPress={() => onSelect(item)}
+                  variant="horizontal"
+                  style={{
+                    marginHorizontal: 16,
+                  }}
+                />
+              )}
+              ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+              keyboardShouldPersistTaps="always"
+            />
+          )}
+        </Pressable>
+      </Pressable>
+    </Modal>
+  );
+};
+
 
 type Props = StaticScreenProps<{
   animeId: string;
@@ -18,6 +110,7 @@ export default function StaffSaveScreen({ route }: Props) {
   const navigation = useNavigation();
   const [staff, setStaff] = useState<Staff>();
   const [form, setForm] = useState<Partial<IStaff>>();
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
     const prepare = async () => {
@@ -32,7 +125,8 @@ export default function StaffSaveScreen({ route }: Props) {
           manga: new Manga({ id: route.params.mangaId }),
         });
       } else {
-        staff = await Staff.findById(route.params.staffId);
+        staff = await Staff.findById(route.params.staffId)
+          .include(['people']);
       }
 
       setStaff(staff);
@@ -62,6 +156,56 @@ export default function StaffSaveScreen({ route }: Props) {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
+        <View style={styles.input}>
+          <InputLabel>
+            Personnalité *
+          </InputLabel>
+
+          <Pressable
+            onPress={() => setIsModalVisible(true)}
+            style={{
+              alignItems: 'center',
+              borderColor: '#ccc',
+              borderRadius: 4,
+              borderWidth: 1,
+              flexDirection: 'row',
+              gap: 12,
+              overflow: 'hidden',
+            }}
+          >
+            {!form.people ? (
+              <View style={{ width: 100, aspectRatio: 1 / 1, backgroundColor: '#ccc' }} />
+            ) : (
+              <>
+                <Image
+                  source={{ uri: form.people.portrait ?? undefined }}
+                  style={{
+                    width: 100,
+                    aspectRatio: 1 / 1,
+                    backgroundColor: '#ccc',
+                  }}
+                />
+
+                <Text>
+                  {form.people.name}
+                </Text>
+              </>
+            )}
+          </Pressable>
+
+          <SelectDestinationModal
+            onSelect={(people) => {
+              setForm((prev) => ({
+                ...prev,
+                people: people,
+              }));
+              setIsModalVisible(false);
+            }}
+            onRequestClose={() => setIsModalVisible(false)}
+            visible={isModalVisible}
+          />
+        </View>
+
         <SelectInput
           label="Role *"
           values={[
