4c900a0ce5b4369213c988927c1d6cab362e41d3
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-20 15:46:06 +0200

    feat(staff-save): display staff save form

--- screens/staff-save/StaffSaveScreen.tsx
+++ screens/staff-save/StaffSaveScreen.tsx
@@ -1,7 +1,8 @@
 import { StaticScreenProps, useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { ActivityIndicator, ScrollView, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import SelectInput from '../../components/atoms/SelectInput';
 import { Anime, Manga, Staff } from '../../models';
 import { IStaff } from '../../models/staff.model';
 
@@ -46,8 +47,40 @@ export default function StaffSaveScreen({ route }: Props) {
     return unsubscribe;
   }, [route.params]);
 
+  if (!staff || !form) {
+    return (
+      <SafeAreaView style={[styles.container, { alignItems: 'center', justifyContent: 'center' }]}>
+        <ActivityIndicator
+          animating
+          color="#000"
+          size="large"
+        />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <SelectInput
+          label="Role *"
+          values={[
+            { label: 'Auteur', value: 'author' },
+            { label: 'Illustrateur', value: 'illustrator' },
+            { label: 'Scénario et dessin', value: 'story_and_art' },
+            { label: 'Licencié', value: 'licensor' },
+            { label: 'Producteur', value: 'producer' },
+            { label: 'Studio', value: 'studio' },
+            { label: 'Créateur original', value: 'original_creator' },
+          ]}
+          selectedValue={form.role}
+          onValueChange={(value) => setForm((prev) => ({
+            ...prev,
+            role: value,
+          }))}
+          style={styles.input}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -55,5 +88,9 @@ export default function StaffSaveScreen({ route }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  input: {
+    marginHorizontal: 16,
+    marginTop: 16,
   },
 });
