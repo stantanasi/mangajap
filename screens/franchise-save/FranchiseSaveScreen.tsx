1bfde26ef21152af8b972a5dbd9766605da4c7a2
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-20 14:47:31 +0200

    feat(franchise-save): display franchise save form

--- screens/franchise-save/FranchiseSaveScreen.tsx
+++ screens/franchise-save/FranchiseSaveScreen.tsx
@@ -1,7 +1,8 @@
 import { StaticScreenProps, useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { ActivityIndicator, ScrollView, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import SelectInput from '../../components/atoms/SelectInput';
 import { Anime, Franchise, Manga } from '../../models';
 import { IFranchise } from '../../models/franchise.model';
 
@@ -47,8 +48,45 @@ export default function FranchiseSaveScreen({ route }: Props) {
     return unsubscribe;
   }, [route.params]);
 
+  if (!franchise || !form) {
+    return (
+      <SafeAreaView style={[styles.container, { alignItems: 'center', justifyContent: 'center' }]}>
+        <ActivityIndicator
+          animating
+          color="#000"
+          size="large"
+        />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <SelectInput
+          label="Role *"
+          values={[
+            { label: 'Adaptation', value: 'adaptation' },
+            { label: 'Univers alternatif', value: 'alternative_setting' },
+            { label: 'Version alternative', value: 'alternative_version' },
+            { label: 'Personnage', value: 'character' },
+            { label: 'Histoire complète', value: 'full_story' },
+            { label: 'Autre', value: 'other' },
+            { label: 'Histoire principale', value: 'parent_story' },
+            { label: 'Préquelle', value: 'prequel' },
+            { label: 'Suite', value: 'sequel' },
+            { label: 'Histoire parallèle', value: 'side_story' },
+            { label: 'Spin-off', value: 'spinoff' },
+            { label: 'Résumé', value: 'summary' },
+          ]}
+          selectedValue={form.role}
+          onValueChange={(value) => setForm((prev) => ({
+            ...prev,
+            role: value,
+          }))}
+          style={styles.input}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -56,5 +94,9 @@ export default function FranchiseSaveScreen({ route }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  input: {
+    marginHorizontal: 16,
+    marginTop: 16,
   },
 });
