6e0971f7e3daeb7e5fb503c2c6d6dd270905dcde
Lory-Stan TANASI <stantanasi@gmail.com>
2025-04-30 13:44:19 +0200

    feat(date-time-picker): implement select date

--- components/atoms/DateTimePicker.tsx
+++ components/atoms/DateTimePicker.tsx
@@ -1,6 +1,10 @@
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import Modal from './Modal';
+
+const DAYS = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+const MONTHS = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
 
 type Props = {
   value: Date;
@@ -23,6 +27,42 @@ export default function DateTimePicker({
     setSelectedDate(value);
   }, [value]);
 
+  const days: Date[] = (() => {
+    const month = activeDate.getMonth();
+    const year = activeDate.getFullYear();
+
+    return Array.from(
+      { length: new Date(year, month + 1, 0).getDate() },
+      (_, i) => new Date(year, month, i + 1)
+    );
+  })();
+
+  const weeks: (Date | null)[][] = (() => {
+    const startDay = days[0].getDay() % 7;
+
+    const allDays: (Date | null)[] = [
+      ...Array.from({ length: startDay }, () => null),
+      ...days,
+    ];
+
+    return Array.from(
+      { length: 6 },
+      (_, i) => {
+        const week = allDays.slice(i * 7, i * 7 + 7);
+        return [
+          ...week,
+          ...Array.from({ length: 7 - week.length }, () => null),
+        ];
+      }
+    );
+  })();
+
+  const isSameDay = (date1: Date, date2: Date) => {
+    return date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate();
+  };
+
   return (
     <Modal
       onRequestClose={onRequestClose}
@@ -32,6 +72,124 @@ export default function DateTimePicker({
       <Text>
         Sélectionner une date
       </Text>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text>
+          {selectedDate?.toLocaleDateString() ?? 'Date sélectionnée'}
+        </Text>
+      </View>
+
+      <View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Pressable style={{ flexDirection: 'row' }}>
+            <Text>
+              {MONTHS[activeDate.getMonth()]} {activeDate.getFullYear()}
+            </Text>
+          </Pressable>
+
+          <View style={{ flexDirection: 'row' }}>
+            <MaterialIcons
+              name="keyboard-arrow-left"
+              color="#000"
+              size={24}
+              onPress={() => setActiveDate((prev) => {
+                const date = new Date(prev);
+                date.setMonth(date.getMonth() - 1);
+                return date;
+              })}
+            />
+
+            <MaterialIcons
+              name="keyboard-arrow-right"
+              color="#000"
+              size={24}
+              onPress={() => setActiveDate((prev) => {
+                const date = new Date(prev);
+                date.setMonth(date.getMonth() + 1);
+                return date;
+              })}
+            />
+          </View>
+        </View>
+
+        <View>
+          <View style={{ flexDirection: 'row' }}>
+            {DAYS.map((day) => (
+              <View
+                key={day}
+                style={{ alignItems: 'center', aspectRatio: 1 / 1, flex: 1, justifyContent: 'center', margin: 2 }}
+              >
+                <Text>
+                  {day.substring(0, 1)}
+                </Text>
+              </View>
+            ))}
+          </View>
+
+          {weeks.map((week, i) => (
+            <View
+              key={i}
+              style={{ flexDirection: 'row' }}
+            >
+              {week.map((day, j) => {
+                if (day === null) {
+                  return (
+                    <View
+                      key={j}
+                      style={{ alignItems: 'center', aspectRatio: 1 / 1, flex: 1, justifyContent: 'center', margin: 2 }}
+                    />
+                  );
+                }
+
+                const isToday = isSameDay(new Date(), day);
+                const isSelected = selectedDate ? isSameDay(selectedDate, day) : false;
+
+                return (
+                  <Pressable
+                    key={j}
+                    onPress={() => setSelectedDate(day)}
+                    style={[{ alignItems: 'center', aspectRatio: 1 / 1, flex: 1, justifyContent: 'center', margin: 2 }, isToday ? {
+                      borderColor: '#4281f5',
+                      borderRadius: 360,
+                      borderWidth: 1,
+                    } : {}, isSelected ? {
+                      backgroundColor: '#4281f5',
+                      borderRadius: 360,
+                    } : {}]}
+                  >
+                    <Text style={[{}, isToday ? {
+                      color: '#4281f5',
+                    } : {}, isSelected ? {
+                      color: '#fff',
+                    } : {}]}>
+                      {day.getDate()}
+                    </Text>
+                  </Pressable>
+                );
+              })}
+            </View>
+          ))}
+        </View>
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+        <Text
+          onPress={() => onRequestClose()}
+        >
+          Annuler
+        </Text>
+
+        <Text
+          onPress={() => {
+            if (!selectedDate) return
+
+            onValueChange(selectedDate);
+            onRequestClose();
+          }}
+        >
+          OK
+        </Text>
+      </View>
     </Modal>
   );
 }
