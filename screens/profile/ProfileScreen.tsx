7c013e39f69d55f8cddba2d52bef55bf8cab4bec
Lory-Stan TANASI <stantanasi@gmail.com>
2026-04-19 19:28:17 -0400

    feat(profile): use Tabs component

--- screens/profile/ProfileScreen.tsx
+++ screens/profile/ProfileScreen.tsx
@@ -1,17 +1,17 @@
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { StaticScreenProps, useNavigation } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
-import { FlatList, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import RefreshControl from '../../components/atoms/RefreshControl';
-import AnimeCard from '../../components/molecules/AnimeCard';
-import MangaCard from '../../components/molecules/MangaCard';
 import LoadingScreen from '../../components/organisms/LoadingScreen';
+import Tabs from '../../components/organisms/Tabs';
 import { useAuth } from '../../contexts/AuthContext';
 import Header from './components/Header';
 import { useProfile } from './hooks/useProfile';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import AnimeTab from './tabs/AnimeTab';
+import MangaTab from './tabs/MangaTab';
 
 type Props = StaticScreenProps<{
   id: string;
@@ -64,268 +64,31 @@ export default function ProfileScreen({ route }: Props) {
           paddingBottom: 16,
         }}
       >
-        <Header
-          isLoading={isLoading}
-          route={route}
-          user={user}
-          followingUser={followingUser}
-          followedByUser={followedByUser}
-        />
-
-        <Text
-          style={{
-            fontSize: 20,
-            fontWeight: 'bold',
-            marginBottom: 6,
-            marginHorizontal: 16,
-            marginTop: 20,
-          }}
-        >
-          Statistiques
-        </Text>
-
-        <FlatList
-          horizontal
-          data={[
-            { label: 'Animé suivis', value: user.followedAnimeCount },
-            { label: 'Épisodes vus', value: user.episodesWatch },
-            { label: 'Manga suivis', value: user.followedMangaCount },
-            { label: 'Tomes lus', value: user.volumesRead },
-            { label: 'Chapitres lus', value: user.chaptersRead },
-          ]}
-          keyExtractor={(item) => item.label}
-          renderItem={({ item }) => (
-            <View
-              style={{
-                minWidth: 200,
-                alignItems: 'center',
-                borderColor: '#ccc',
-                borderRadius: 4,
-                borderWidth: 1,
-                gap: 6,
-                paddingHorizontal: 16,
-                paddingVertical: 6,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 13,
-                  fontWeight: 'bold',
-                  marginHorizontal: 10,
-                }}
-              >
-                {item.label}
-              </Text>
-
-              <View style={{ width: '100%', height: 1, backgroundColor: '#ccc' }} />
-
-              <Text
-                style={{
-                  fontSize: 24,
-                  fontWeight: 'bold',
-                  marginHorizontal: 10,
-                }}
-              >
-                {item.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}
-              </Text>
-            </View>
-          )}
-          ItemSeparatorComponent={() => <View style={{ width: 6 }} />}
-          ListHeaderComponent={() => <View style={{ width: 16 }} />}
-          ListFooterComponent={() => <View style={{ width: 16 }} />}
-        />
-
-        <Pressable
-          onPress={() => navigation.navigate('ProfileLibrary', { type: 'anime-library', userId: user.id })}
-          style={{
-            alignItems: 'center',
-            flexDirection: 'row',
-            marginBottom: 6,
-            marginHorizontal: 16,
-            marginTop: 20,
-          }}
-        >
-          <Text
-            style={{
-              flex: 1,
-              fontSize: 20,
-              fontWeight: 'bold',
-            }}
-          >
-            Anime
-          </Text>
-
-          <MaterialIcons
-            name="keyboard-arrow-right"
-            size={24}
-            color="#000"
-          />
-        </Pressable>
-
-        <FlatList
-          horizontal
-          data={user['anime-library']?.map((entry) => entry.anime?.copy({
-            'anime-entry': entry,
-          })).filter((anime) => !!anime)}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <AnimeCard
+        <Tabs.Container
+          header={() => (
+            <Header
               isLoading={isLoading}
-              anime={item}
-              onPress={() => navigation.navigate('Anime', { id: item.id })}
-              showCheckbox={false}
+              route={route}
+              user={user}
+              followingUser={followingUser}
+              followedByUser={followedByUser}
             />
           )}
-          ItemSeparatorComponent={() => <View style={{ width: 6 }} />}
-          ListHeaderComponent={() => <View style={{ width: 16 }} />}
-          ListFooterComponent={() => <View style={{ width: 16 }} />}
-        />
-
-        {(user['anime-favorites']?.length ?? 0) > 0 ? (
-          <>
-            <Pressable
-              onPress={() => navigation.navigate('ProfileLibrary', { type: 'anime-favorites', userId: user.id })}
-              style={{
-                alignItems: 'center',
-                flexDirection: 'row',
-                marginBottom: 6,
-                marginHorizontal: 16,
-                marginTop: 20,
-              }}
-            >
-              <Text
-                style={{
-                  flex: 1,
-                  fontSize: 20,
-                  fontWeight: 'bold',
-                }}
-              >
-                Anime préférés
-              </Text>
-
-              <MaterialIcons
-                name="keyboard-arrow-right"
-                size={24}
-                color="#000"
-              />
-            </Pressable>
-
-            <FlatList
-              horizontal
-              data={user['anime-favorites']?.map((entry) => entry.anime?.copy({
-                'anime-entry': entry,
-              })).filter((anime) => !!anime)}
-              keyExtractor={(item) => item.id}
-              renderItem={({ item }) => (
-                <AnimeCard
-                  isLoading={isLoading}
-                  anime={item}
-                  onPress={() => navigation.navigate('Anime', { id: item.id })}
-                  showCheckbox={false}
-                />
-              )}
-              ItemSeparatorComponent={() => <View style={{ width: 6 }} />}
-              ListHeaderComponent={() => <View style={{ width: 16 }} />}
-              ListFooterComponent={() => <View style={{ width: 16 }} />}
-            />
-          </>
-        ) : null}
-
-        <Pressable
-          onPress={() => navigation.navigate('ProfileLibrary', { type: 'manga-library', userId: user.id })}
-          style={{
-            alignItems: 'center',
-            flexDirection: 'row',
-            marginBottom: 6,
-            marginHorizontal: 16,
-            marginTop: 20,
-          }}
         >
-          <Text
-            style={{
-              flex: 1,
-              fontSize: 20,
-              fontWeight: 'bold',
-            }}
-          >
-            Manga
-          </Text>
-
-          <MaterialIcons
-            name="keyboard-arrow-right"
-            size={24}
-            color="#000"
-          />
-        </Pressable>
-
-        <FlatList
-          horizontal
-          data={user['manga-library']?.map((entry) => entry.manga?.copy({
-            'manga-entry': entry,
-          })).filter((manga) => !!manga)}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <MangaCard
+          <Tabs.Tab name="Anime">
+            <AnimeTab
               isLoading={isLoading}
-              manga={item}
-              onPress={() => navigation.navigate('Manga', { id: item.id })}
-              showCheckbox={false}
+              user={user}
             />
-          )}
-          ItemSeparatorComponent={() => <View style={{ width: 6 }} />}
-          ListHeaderComponent={() => <View style={{ width: 16 }} />}
-          ListFooterComponent={() => <View style={{ width: 16 }} />}
-        />
+          </Tabs.Tab>
 
-        {(user['manga-favorites']?.length ?? 0) > 0 ? (
-          <>
-            <Pressable
-              onPress={() => navigation.navigate('ProfileLibrary', { type: 'manga-favorites', userId: user.id })}
-              style={{
-                alignItems: 'center',
-                flexDirection: 'row',
-                marginBottom: 6,
-                marginHorizontal: 16,
-                marginTop: 20,
-              }}
-            >
-              <Text
-                style={{
-                  flex: 1,
-                  fontSize: 20,
-                  fontWeight: 'bold',
-                }}
-              >
-                Manga préférés
-              </Text>
-
-              <MaterialIcons
-                name="keyboard-arrow-right"
-                size={24}
-                color="#000"
-              />
-            </Pressable>
-
-            <FlatList
-              horizontal
-              data={user['manga-favorites']?.map((entry) => entry.manga?.copy({
-                'manga-entry': entry,
-              })).filter((manga) => !!manga)}
-              keyExtractor={(item) => item.id}
-              renderItem={({ item }) => (
-                <MangaCard
-                  isLoading={isLoading}
-                  manga={item}
-                  onPress={() => navigation.navigate('Manga', { id: item.id })}
-                  showCheckbox={false}
-                />
-              )}
-              ItemSeparatorComponent={() => <View style={{ width: 6 }} />}
-              ListHeaderComponent={() => <View style={{ width: 16 }} />}
-              ListFooterComponent={() => <View style={{ width: 16 }} />}
+          <Tabs.Tab name="Manga">
+            <MangaTab
+              isLoading={isLoading}
+              user={user}
             />
-          </>
-        ) : null}
+          </Tabs.Tab>
+        </Tabs.Container>
       </ScrollView>
 
       <RefreshControl refreshing={isLoading} />
@@ -336,50 +99,5 @@ export default function ProfileScreen({ route }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  avatar: {
-    width: 100,
-    height: 100,
-    alignSelf: 'center',
-    backgroundColor: '#ccc',
-    borderRadius: 360,
-    marginHorizontal: 16,
-    marginTop: 16,
-  },
-  username: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginTop: 16,
-    paddingHorizontal: 16,
-    textAlign: 'center',
-  },
-  pseudo: {
-    color: '#a1a1a1',
-    marginHorizontal: 16,
-    textAlign: 'center',
-  },
-  bio: {
-    marginHorizontal: 16,
-    marginTop: 12,
-    textAlign: 'center',
-  },
-  metas: {
-    flexDirection: 'row',
-    gap: 8,
-    marginHorizontal: 48,
-    marginTop: 16,
-  },
-  meta: {
-    alignItems: 'center',
-    flex: 1,
-  },
-  metaValue: {
-    fontSize: 17,
-    fontWeight: 'bold',
-  },
-  metaLabel: {},
-  metaDivider: {
-    width: 1,
-    backgroundColor: '#ccc',
   },
 });
