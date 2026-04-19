7c67df19147ef524f940e0d4ed0666ea0f780894
Lory-Stan TANASI <stantanasi@gmail.com>
2026-04-19 19:28:42 -0400

    feat(franchise-save:select-destination-modal): use Tabs component

--- screens/franchise-save/modals/SelectDestinationModal.tsx
+++ screens/franchise-save/modals/SelectDestinationModal.tsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, View } from 'react-native';
 import Modal from '../../../components/atoms/Modal';
 import SearchBar from '../../../components/atoms/SearchBar';
-import TabBar from '../../../components/atoms/TabBar';
 import AnimeCard from '../../../components/molecules/AnimeCard';
 import MangaCard from '../../../components/molecules/MangaCard';
+import Tabs from '../../../components/organisms/Tabs';
 import { Anime, Manga } from '../../../models';
 import { useAppDispatch, useAppSelector } from '../../../redux/store';
 import notify from '../../../utils/notify';
@@ -20,7 +20,6 @@ export default function SelectDestinationModal({ onSelect, onRequestClose, visib
   const [isLoading, setIsLoading] = useState(true);
   const [animeIds, setAnimeIds] = useState<string[]>([]);
   const [mangaIds, setMangaIds] = useState<string[]>([]);
-  const [selectedTab, setSelectedTab] = useState<'anime' | 'manga'>('anime');
 
   const animes = useAppSelector((state) => {
     return Anime.redux.selectors.selectByIds(state, animeIds);
@@ -40,124 +39,113 @@ export default function SelectDestinationModal({ onSelect, onRequestClose, visib
       visible={visible}
       style={{ height: '90%' }}
     >
-      <SearchBar
-        onChangeText={() => {
-          setAnimeIds([]);
-          setMangaIds([]);
-        }}
-        onSearch={(query) => {
-          setAnimeIds([]);
-          setMangaIds([]);
+      <Tabs.Container
+        header={() => (
+          <View>
+            <SearchBar
+              onChangeText={() => {
+                setAnimeIds([]);
+                setMangaIds([]);
+              }}
+              onSearch={(query) => {
+                setAnimeIds([]);
+                setMangaIds([]);
 
-          setIsLoading(true);
-          Promise.all([
-            Anime.find({ query: query }),
-            Manga.find({ query: query }),
-          ])
-            .then(([animes, mangas]) => {
-              dispatch(Anime.redux.actions.setMany(animes));
-              dispatch(Manga.redux.actions.setMany(mangas));
+                setIsLoading(true);
+                Promise.all([
+                  Anime.find({ query: query }),
+                  Manga.find({ query: query }),
+                ])
+                  .then(([animes, mangas]) => {
+                    dispatch(Anime.redux.actions.setMany(animes));
+                    dispatch(Manga.redux.actions.setMany(mangas));
 
-              setAnimeIds(animes.map((anime) => anime.id));
-              setMangaIds(mangas.map((manga) => manga.id));
-            })
-            .catch((err) => notify.error('search_load', err))
-            .finally(() => setIsLoading(false));
-        }}
-        delay={500}
-        style={{
-          backgroundColor: undefined,
-          borderColor: '#ccc',
-          borderRadius: 4,
-          borderWidth: 1,
-          marginHorizontal: 16,
-          marginTop: 16,
-          paddingHorizontal: 10,
-        }}
-      />
+                    setAnimeIds(animes.map((anime) => anime.id));
+                    setMangaIds(mangas.map((manga) => manga.id));
+                  })
+                  .catch((err) => notify.error('search_load', err))
+                  .finally(() => setIsLoading(false));
+              }}
+              delay={500}
+              style={{
+                backgroundColor: undefined,
+                borderColor: '#ccc',
+                borderRadius: 4,
+                borderWidth: 1,
+                marginHorizontal: 16,
+                marginTop: 16,
+                paddingHorizontal: 10,
+              }}
+            />
 
-      <TabBar
-        selected={selectedTab}
-        tabs={[
-          { key: 'anime', title: 'Animé' },
-          { key: 'manga', title: 'Manga' },
-        ]}
-        onTabChange={(key) => setSelectedTab(key)}
-      />
-
-      <View
-        style={{
-          display: selectedTab === 'anime' ? 'flex' : 'none',
-          flex: 1,
-        }}
-      >
-        {isLoading ? (
-          <ActivityIndicator
-            animating
-            color="#000"
-            size="large"
-            style={{ margin: 16 }}
-          />
-        ) : (
-          <FlatList
-            data={animes}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => (
-              <AnimeCard
-                isLoading={!animes}
-                anime={item}
-                onPress={() => onSelect(item)}
-                variant="horizontal"
-                showCheckbox={false}
-                style={{
-                  marginHorizontal: 16,
-                }}
-              />
-            )}
-            ListHeaderComponent={() => <View style={{ height: 16 }} />}
-            ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
-            ListFooterComponent={() => <View style={{ height: 16 }} />}
-            keyboardShouldPersistTaps="always"
-          />
+            <Tabs.Bar />
+          </View>
         )}
-      </View>
-
-      <View
-        style={{
-          display: selectedTab === 'manga' ? 'flex' : 'none',
-          flex: 1,
-        }}
       >
-        {isLoading ? (
-          <ActivityIndicator
-            animating
-            color="#000"
-            size="large"
-            style={{ margin: 16 }}
-          />
-        ) : (
-          <FlatList
-            data={mangas}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => (
-              <MangaCard
-                isLoading={!mangas}
-                manga={item}
-                onPress={() => onSelect(item)}
-                variant="horizontal"
-                showCheckbox={false}
-                style={{
-                  marginHorizontal: 16,
-                }}
-              />
-            )}
-            ListHeaderComponent={() => <View style={{ height: 16 }} />}
-            ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
-            ListFooterComponent={() => <View style={{ height: 16 }} />}
-            keyboardShouldPersistTaps="always"
-          />
-        )}
-      </View>
+        <Tabs.Tab name="Anime">
+          {isLoading ? (
+            <ActivityIndicator
+              animating
+              color="#000"
+              size="large"
+              style={{ margin: 16 }}
+            />
+          ) : (
+            <FlatList
+              data={animes}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => (
+                <AnimeCard
+                  isLoading={!animes}
+                  anime={item}
+                  onPress={() => onSelect(item)}
+                  variant="horizontal"
+                  showCheckbox={false}
+                  style={{
+                    marginHorizontal: 16,
+                  }}
+                />
+              )}
+              ListHeaderComponent={() => <View style={{ height: 16 }} />}
+              ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+              ListFooterComponent={() => <View style={{ height: 16 }} />}
+              keyboardShouldPersistTaps="always"
+            />
+          )}
+        </Tabs.Tab>
+
+        <Tabs.Tab name="Manga">
+          {isLoading ? (
+            <ActivityIndicator
+              animating
+              color="#000"
+              size="large"
+              style={{ margin: 16 }}
+            />
+          ) : (
+            <FlatList
+              data={mangas}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => (
+                <MangaCard
+                  isLoading={!mangas}
+                  manga={item}
+                  onPress={() => onSelect(item)}
+                  variant="horizontal"
+                  showCheckbox={false}
+                  style={{
+                    marginHorizontal: 16,
+                  }}
+                />
+              )}
+              ListHeaderComponent={() => <View style={{ height: 16 }} />}
+              ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+              ListFooterComponent={() => <View style={{ height: 16 }} />}
+              keyboardShouldPersistTaps="always"
+            />
+          )}
+        </Tabs.Tab>
+      </Tabs.Container>
     </Modal>
   );
 };
